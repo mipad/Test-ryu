632e657f1d12687464477e7a84fb75cae7ecd736
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-04 19:49:08 +0800

    Update NativeHelpers.kt
    Try adding screens like 16:9

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/NativeHelpers.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/NativeHelpers.kt
@@ -5,6 +5,9 @@ import android.view.Surface
 class NativeHelpers {
 
     companion object {
+        const val ASPECT_RATIO_STRETCH = 0
+        const val ASPECT_RATIO_16_9 = 1
+
         val instance = NativeHelpers()
 
         init {
@@ -12,16 +15,19 @@ class NativeHelpers {
         }
     }
 
+    // 新增接口
+    external fun setAspectRatio(nativeWindow: Long, ratioMode: Int)
+    external fun getCurrentAspectRatio(): Int
+
+    // 原有接口保持不变
     external fun releaseNativeWindow(window: Long)
     external fun getCreateSurfacePtr(): Long
     external fun getNativeWindow(surface: Surface): Long
-
     external fun loadDriver(
         nativeLibPath: String,
         privateAppsPath: String,
         driverName: String
     ): Long
-
     external fun setTurboMode(enable: Boolean)
     external fun getMaxSwapInterval(nativeWindow: Long): Int
     external fun getMinSwapInterval(nativeWindow: Long): Int
