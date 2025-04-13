411dd528c3f6a501fdd03ae90377f0673a9c725f
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-13 14:54:34 +0800

    Create OpenALLibraryNameContainer.cs

---
+++ src/Ryujinx.Audio.Backends.OpenAL/OpenALLibraryNameContainer.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Ryujinx.Audio.Backends.OpenAL
+{
+    public static class OpenALLibraryNameContainer
+    {
+        public static string OverridePath { get; set; } = string.Empty;
+    }
+}
