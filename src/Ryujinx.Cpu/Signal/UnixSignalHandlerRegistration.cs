3323c8938ae13f36229bb2ffbe1c0985a2b35dd9
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-19 04:00:59 +0800

    Update UnixSignalHandlerRegistration.cs

--- src/Ryujinx.Cpu/Signal/UnixSignalHandlerRegistration.cs
+++ src/Ryujinx.Cpu/Signal/UnixSignalHandlerRegistration.cs
@@ -64,90 +64,98 @@ namespace Ryujinx.Cpu.Signal
         private static partial int sigaltstack(ref Stack ss, out Stack oldSs);
 
         public static SigAction GetSegfaultExceptionHandler()
+  {
+      int result;
+      SigAction old;
+
+      if (OperatingSystem.IsAndroid())
+      {
+          #if ANDROID
+          result = sigaction(SIGSEGV, IntPtr.Zero, out SigActionBionic tmp);
+
+          old = new SigAction
+          {
+              sa_handler = tmp.sa_handler,
+              sa_mask = tmp.sa_mask,
+              sa_flags = tmp.sa_flags,
+              sa_restorer = tmp.sa_restorer
+          };
+          #else
+          throw new PlatformNotSupportedException("Android-only API is not available.");
+          #endif
+      }
+      else
+      {
+          result = sigaction(SIGSEGV, IntPtr.Zero, out old);
+      }
+
+      if (result != 0)
+      {
+          throw new SystemException($"Could not get SIGSEGV sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
+      }
+
+      return old;
+  }
+
+        public static SigAction RegisterExceptionHandler(IntPtr action)
+{
+    int result;
+    SigAction old;
+
+    if (OperatingSystem.IsAndroid())
+    {
+        #if ANDROID
+        SigActionBionic sig = new()
         {
-            int result;
-            SigAction old;
+            sa_handler = action,
+            sa_flags = SA_SIGINFO | SA_ONSTACK,
+        };
 
-            if (Ryujinx.Common.PlatformInfo.IsBionic)
-            {
-                result = sigaction(SIGSEGV, IntPtr.Zero, out SigActionBionic tmp);
+        sigemptyset(ref sig.sa_mask);
 
-                old = new SigAction
-                {
-                    sa_handler = tmp.sa_handler,
-                    sa_mask = tmp.sa_mask,
-                    sa_flags = tmp.sa_flags,
-                    sa_restorer = tmp.sa_restorer
-                };
-            }
-            else
-            {
-                result = sigaction(SIGSEGV, IntPtr.Zero, out old);
-            }
+        result = sigaction(SIGSEGV, ref sig, out SigActionBionic tmp);
+
+        old = new SigAction
+        {
+            sa_handler = tmp.sa_handler,
+            sa_mask = tmp.sa_mask,
+            sa_flags = tmp.sa_flags,
+            sa_restorer = tmp.sa_restorer
+        };
+        #else
+        throw new PlatformNotSupportedException("Android-only API is not available.");
+        #endif
+    }
+    else
+    {
+        SigAction sig = new SigAction
+        {
+            sa_handler = action,
+            sa_flags = SA_SIGINFO | SA_ONSTACK,
+        };
+
+        sigemptyset(ref sig.sa_mask);
+
+        result = sigaction(SIGSEGV, ref sig, out old);
+
+        if (result != 0)
+        {
+            throw new SystemException($"Could not register SIGSEGV sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
+        }
+
+        if (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS())
+        {
+            result = sigaction(SIGBUS, ref sig, out _);
 
             if (result != 0)
             {
-                throw new SystemException($"Could not get SIGSEGV sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
+                throw new SystemException($"Could not register SIGBUS sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
             }
-
-            return old;
         }
+    }
 
-        public static SigAction RegisterExceptionHandler(IntPtr action)
-        {
-            int result;
-            SigAction old;
-
-            if (Ryujinx.Common.PlatformInfo.IsBionic)
-            {
-                SigActionBionic sig = new()
-                {
-                    sa_handler = action,
-                    sa_flags = SA_SIGINFO | SA_ONSTACK,
-                };
-
-                sigemptyset(ref sig.sa_mask);
-
-                result = sigaction(SIGSEGV, ref sig, out SigActionBionic tmp);
-
-                old = new SigAction
-                {
-                    sa_handler = tmp.sa_handler,
-                    sa_mask = tmp.sa_mask,
-                    sa_flags = tmp.sa_flags,
-                    sa_restorer = tmp.sa_restorer
-                };
-            }
-            else
-            {
-                SigAction sig = new SigAction
-                {
-                    sa_handler = action,
-                    sa_flags = SA_SIGINFO | SA_ONSTACK,
-                };
-
-                sigemptyset(ref sig.sa_mask);
-
-                result = sigaction(SIGSEGV, ref sig, out old);
-
-                if (result != 0)
-                {
-                    throw new SystemException($"Could not register SIGSEGV sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
-                }
-
-                if (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS())
-                {
-                    result = sigaction(SIGBUS, ref sig, out _);
-
-                    if (result != 0)
-                    {
-                        throw new SystemException($"Could not register SIGBUS sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
-                    }
-                }
-            }
-
-            return old;
-        }
+    return old;
+}
 
         public static void RegisterAlternateStack(IntPtr stackPtr, ulong stackSize)
         {
@@ -185,8 +193,9 @@ namespace Ryujinx.Cpu.Signal
         {
             int result;
 
-            if (Ryujinx.Common.PlatformInfo.IsBionic)
+            if (OperatingSystem.IsAndroid())
             {
+                #if ANDROID
                 SigActionBionic sig = new()
                 {
                     sa_handler = action,
@@ -206,6 +215,7 @@ namespace Ryujinx.Cpu.Signal
                 {
                     throw new SystemException($"Could not register SIG{sigNum} sigaction. Error: {Marshal.GetLastPInvokeErrorMessage()}");
                 }
+                #endif
             }
             else
             {
@@ -232,30 +242,34 @@ namespace Ryujinx.Cpu.Signal
         }
 
         public static bool RestoreExceptionHandler(SigAction oldAction)
-        {
-            if (Ryujinx.Common.PlatformInfo.IsBionic)
-            {
-                SigActionBionic tmp = new SigActionBionic
-                {
-                    sa_handler = oldAction.sa_handler,
-                    sa_mask = oldAction.sa_mask,
-                    sa_flags = oldAction.sa_flags,
-                    sa_restorer = oldAction.sa_restorer
-                };
+  {
+      if (OperatingSystem.IsAndroid())
+      {
+          #if ANDROID
+          SigActionBionic tmp = new SigActionBionic
+          {
+              sa_handler = oldAction.sa_handler,
+              sa_mask = oldAction.sa_mask,
+              sa_flags = oldAction.sa_flags,
+              sa_restorer = oldAction.sa_restorer
+          };
 
-                return sigaction(SIGSEGV, ref tmp, out SigActionBionic _) == 0;
-            }
-            else
-            {
-                bool success = sigaction(SIGSEGV, ref oldAction, out SigAction _) == 0;
+          return sigaction(SIGSEGV, ref tmp, out SigActionBionic _) == 0;
+          #else
+          throw new PlatformNotSupportedException("Android-only API is not available.");
+          #endif
+      }
+      else
+      {
+          bool success = sigaction(SIGSEGV, ref oldAction, out SigAction _) == 0;
 
-                if (success && (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS()))
-                {
-                    success = sigaction(SIGBUS, ref oldAction, out SigAction _) == 0;
-                }
+          if (success && (OperatingSystem.IsMacOS() || OperatingSystem.IsIOS()))
+          {
+              success = sigaction(SIGBUS, ref oldAction, out SigAction _) == 0;
+          }
 
-                return success;
-            }
+          return success;
+          }
         }
-    }
+     }
 }
