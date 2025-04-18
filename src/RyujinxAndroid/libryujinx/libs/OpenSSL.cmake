85745307499c42acf52a425e52e280a07a6627a5
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-18 20:30:36 +0800

    Update OpenSSL.cmake

--- src/RyujinxAndroid/libryujinx/libs/OpenSSL.cmake
+++ src/RyujinxAndroid/libryujinx/libs/OpenSSL.cmake
@@ -1,51 +1,54 @@
 include(ExternalProject)
+
 find_package(Perl 5 REQUIRED)
 
-# 确保 NDK 路径正确传递
-set(ENV{ANDROID_NDK_ROOT} ${CMAKE_ANDROID_NDK})
-set(PROJECT_ENV
-    "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}"
-    "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}"
-)
+set(PROJECT_ENV "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}")
 
-# 架构名称转换
-if(CMAKE_ANDROID_ARCH MATCHES "arm64.*")
-    set(OPENSSL_ARCH "android-arm64")
-elseif(CMAKE_ANDROID_ARCH MATCHES "x86_64")
-    set(OPENSSL_ARCH "android-x86_64")
-else()
-    message(FATAL_ERROR "Unsupported architecture: ${CMAKE_ANDROID_ARCH}")
-endif()
+if (CMAKE_HOST_WIN32)
+    set(ProgramFiles_x86 "$ENV{ProgramFiles\(x86\)}")
+    # https://github.com/microsoft/vswhere/wiki/Find-MSBuild
+    cmake_path(APPEND VSWHERE_BIN "${ProgramFiles_x86}" "Microsoft Visual Studio" "Installer" "vswhere.exe")
+    # FIXME: Hardcoded architecture, no way to specify the MSVC version
+    execute_process(
+            COMMAND ${VSWHERE_BIN} "-latest" "-find" "VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64\\nmake.exe"
+            OUTPUT_VARIABLE NMAKE_PATHS_OUTPUT
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            COMMAND_ERROR_IS_FATAL ANY
+    )
+    string(REPLACE "\n" ";" NMAKE_PATH_LIST "${NMAKE_PATHS_OUTPUT}")
+    list(GET NMAKE_PATH_LIST 0 NMAKE_PATH)
+    cmake_path(NATIVE_PATH NMAKE_PATH NORMALIZE MAKE_COMMAND)
 
-# Windows 特定配置
-if(CMAKE_HOST_WIN32)
-    find_program(NMAKE_EXE nmake REQUIRED)
-    set(MAKE_COMMAND ${NMAKE_EXE})
-    # 确保 Perl 路径无空格
-    string(REPLACE " " "^ " PERL_EXECUTABLE_FIXED ${PERL_EXECUTABLE})
-    set(PROJECT_CFG_PREFIX ${PERL_EXECUTABLE_FIXED})
-    # 使用分号作为路径分隔符
-    list(APPEND PROJECT_ENV "Path=${ANDROID_TOOLCHAIN_ROOT}/bin;$ENV{Path}")
-else()
-    find_program(MAKE_COMMAND NAMES make REQUIRED)
     set(PROJECT_CFG_PREFIX ${PERL_EXECUTABLE})
-endif()
+    # Deal with semicolon-separated lists
+    set(PROJECT_PATH_LIST $ENV{Path})
+    cmake_path(CONVERT "${ANDROID_TOOLCHAIN_ROOT}\\bin" TO_NATIVE_PATH_LIST ANDROID_TOOLCHAIN_BIN NORMALIZE)
+    list(PREPEND PROJECT_PATH_LIST "${ANDROID_TOOLCHAIN_BIN}")
+    # Replace semicolons with "|"
+    list(JOIN PROJECT_PATH_LIST "|" PROJECT_PATH_STRING)
+    # Add the modified PATH string to PROJECT_ENV
+    list(APPEND PROJECT_ENV "Path=${PROJECT_PATH_STRING}")
+elseif (CMAKE_HOST_UNIX)
+    find_program(MAKE_COMMAND NAMES make REQUIRED)
+    list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
+else ()
+    message(WARNING "Host system (${CMAKE_HOST_SYSTEM_NAME}) not supported. Treating as unix.")
+    find_program(MAKE_COMMAND NAMES make REQUIRED)
+    list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
+endif ()
 
 ExternalProject_Add(
-    openssl
-    GIT_REPOSITORY https://github.com/openssl/openssl.git
-    GIT_TAG openssl-3.2.1  # 使用标签代替固定 commit
-    CONFIGURE_COMMAND
-        ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
-        ${PROJECT_CFG_PREFIX} <SOURCE_DIR>/Configure
-            ${OPENSSL_ARCH}
-            -D__ANDROID_API__=${CMAKE_SYSTEM_VERSION}
-            --prefix=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
-            --openssldir=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/ssl
-            no-shared  # 强制生成静态库
-    BUILD_COMMAND
-        ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
-        ${MAKE_COMMAND} -j8
-    INSTALL_COMMAND
-        ${MAKE_COMMAND} install_dev  # 安装头文件和静态库
+        openssl
+        GIT_REPOSITORY              https://github.com/openssl/openssl.git
+        GIT_TAG                     a7e992847de83aa36be0c399c89db3fb827b0be2 # openssl-3.2.1
+        LIST_SEPARATOR              "|"
+        CONFIGURE_COMMAND           ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
+                                    ${PROJECT_CFG_PREFIX} <SOURCE_DIR>/Configure
+                                    android-${CMAKE_ANDROID_ARCH}
+                                    -D__ANDROID_API_=${CMAKE_SYSTEM_VERSION}
+                                    --prefix=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+                                    --libdir=""
+        BUILD_COMMAND               ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
+                                    ${MAKE_COMMAND}
+        INSTALL_COMMAND             ${MAKE_COMMAND} install_runtime_libs
 )
