7ffb59542020e671514473fe01100c29a77f7b65
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-23 01:36:31 +0800

    Update SystemClockContext.cs

--- src/Ryujinx.HLE/HOS/Services/Time/Clock/Types/SystemClockContext.cs
+++ src/Ryujinx.HLE/HOS/Services/Time/Clock/Types/SystemClockContext.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 namespace Ryujinx.HLE.HOS.Services.Time.Clock
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    struct SystemClockContext
+    public struct SystemClockContext // 添加 public 修饰符
     {
         public long Offset;
         public SteadyClockTimePoint SteadyTimePoint;
