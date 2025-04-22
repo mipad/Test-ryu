6aad77ca8a95b8b8aba5926a613cb2889a3c3f1c
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-23 01:35:53 +0800

    Update SteadyClockTimePoint.cs

--- src/Ryujinx.HLE/HOS/Services/Time/Clock/Types/SteadyClockTimePoint.cs
+++ src/Ryujinx.HLE/HOS/Services/Time/Clock/Types/SteadyClockTimePoint.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 namespace Ryujinx.HLE.HOS.Services.Time.Clock
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    struct SteadyClockTimePoint
+    public struct SteadyClockTimePoint // 添加 public 修饰符
     {
         public long TimePoint;
         public UInt128 ClockSourceId;
@@ -19,7 +19,6 @@ namespace Ryujinx.HLE.HOS.Services.Time.Clock
                 try
                 {
                     outSpan = checked(other.TimePoint - TimePoint);
-
                     return ResultCode.Success;
                 }
                 catch (OverflowException)
