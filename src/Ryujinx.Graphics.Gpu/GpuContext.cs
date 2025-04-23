19a31b6f9b8efb1428b7ab16231453b6e10a73af
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-24 01:59:25 +0800

    Update GpuContext.cs

--- src/Ryujinx.Graphics.Gpu/GpuContext.cs
+++ src/Ryujinx.Graphics.Gpu/GpuContext.cs
@@ -10,6 +10,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Ryujinx.Graphics.Gpu.Image;
 
 namespace Ryujinx.Graphics.Gpu
 {
