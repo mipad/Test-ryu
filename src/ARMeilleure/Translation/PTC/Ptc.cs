eb6a7b9fead69287afe8c25c67a6866c66f24a67
LotP1 <68976644+lotp1@users.noreply.github.com>
2025-04-22 21:42:41 -0500

    reset infoStreams when the cache is invalid

--- src/ARMeilleure/Translation/PTC/Ptc.cs
+++ src/ARMeilleure/Translation/PTC/Ptc.cs
@@ -563,6 +563,9 @@ namespace ARMeilleure.Translation.PTC
         {
             if (AreCarriersEmpty() || ContainsBlacklistedFunctions())
             {
+                _infosStream.SetLength(0);
+                _relocsStream.SetLength(0);
+                _unwindInfosStream.SetLength(0);
                 return;
             }
 
