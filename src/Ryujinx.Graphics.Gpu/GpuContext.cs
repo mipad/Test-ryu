82a6e277e9661ed862be2936ab58df9a57acafe0
shuyi <54129575+mipad@users.noreply.github.com>
2025-04-24 03:04:35 +0800

    Update GpuContext.cs

--- src/Ryujinx.Graphics.Gpu/GpuContext.cs
+++ src/Ryujinx.Graphics.Gpu/GpuContext.cs
@@ -118,7 +118,7 @@ namespace Ryujinx.Graphics.Gpu
         /// </summary>
         /// <param name="renderer">Host renderer</param>
         // 新增方法（插入到类中）
-public void LoadTextureSafe(Texture texture, byte[] data)
+public void LoadTextureSafe(Ryujinx.Graphics.GAL.Texture texture, byte[] data)
 {
     try
     {
