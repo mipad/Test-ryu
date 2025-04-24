4bcfae59052d414f2b5210357eddb2a04d2f5185
LotP1 <68976644+lotp1@users.noreply.github.com>
2025-04-24 12:48:22 -0500

    Reset PPTC Carriers on invalidation

--- src/ARMeilleure/Translation/PTC/Ptc.cs
+++ src/ARMeilleure/Translation/PTC/Ptc.cs
@@ -563,9 +563,7 @@ namespace ARMeilleure.Translation.PTC
         {
             if (AreCarriersEmpty() || ContainsBlacklistedFunctions())
             {
-                _infosStream.SetLength(0);
-                _relocsStream.SetLength(0);
-                _unwindInfosStream.SetLength(0);
+                ResetCarriersIfNeeded();
                 return;
             }
 
