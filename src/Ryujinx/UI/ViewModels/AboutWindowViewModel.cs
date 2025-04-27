2f48a5007a98b0080ef47a7a98e944689e8f6cf3
KeatonTheBot <onikeaton@gmail.com>
2025-04-26 20:53:19 -0500

    Update 'About' window

--- src/Ryujinx/UI/ViewModels/AboutWindowViewModel.cs
+++ src/Ryujinx/UI/ViewModels/AboutWindowViewModel.cs
@@ -82,8 +82,10 @@ namespace Ryujinx.Ava.UI.ViewModels
             }
         }
 
-        public string Developers => LocaleManager.Instance.UpdateAndGetDynamicValue(LocaleKeys.AboutPageDeveloperListMore, "gdkchan, Ac_K, marysaka, rip in peri peri, LDj3SNuD, emmaus, Thealexbarney, GoffyDude, TSRBerry, IsaacMarovitz");
+        public string FormerDevelopers => LocaleManager.Instance.UpdateAndGetDynamicValue(LocaleKeys.AboutPageDeveloperListMore, "gdkchan, Ac_K, marysaka, rip in peri peri, LDj3SNuD, emmaus, Thealexbarney, GoffyDude, TSRBerry, IsaacMarovitz");
 
+        public string Developers => LocaleManager.Instance.UpdateAndGetDynamicValue(LocaleKeys.AboutPageDeveloperListMore, "KeatonTheBot");
+        
         public AboutWindowViewModel()
         {
             Version = Program.Version;
