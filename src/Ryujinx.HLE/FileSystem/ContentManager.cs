d748d5fcd4ca2ee3c5f4b54371b92619fa188335
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-01 16:02:32 +0800

    Update ContentManager.cs

--- src/Ryujinx.HLE/FileSystem/ContentManager.cs
+++ src/Ryujinx.HLE/FileSystem/ContentManager.cs
@@ -56,7 +56,7 @@ namespace Ryujinx.HLE.FileSystem
 
         private readonly VirtualFileSystem _virtualFileSystem;
 
-        private readonly Lock _lock = new();
+        private readonly object _lock = new();
 
         public ContentManager(VirtualFileSystem virtualFileSystem)
         {
