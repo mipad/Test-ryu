568283a4d5284386376b5f06b7c9cc324f7a2d4b
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-02 15:04:47 +0800

    Update LibRyujinx.cs

--- src/LibRyujinx/LibRyujinx.cs
+++ src/LibRyujinx/LibRyujinx.cs
@@ -761,7 +761,7 @@ namespace LibRyujinx
     hostUIHandler: HostUiHandler,
     systemLanguage: systemLanguage,
     region: regionCode,
-    vSyncMode: enableVsync,
+    vSyncMode: VSyncMode,
     enableDockedMode: enableDockedMode,
     enablePtc: enablePtc,
     enableInternetAccess: enableInternetAccess,
