8dc74ef3f7f66e69c75591412627a1860718bdbe
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-02 11:41:29 +0800

    Update VSyncMode.cs

--- src/Ryujinx.Graphics.GAL/VSyncMode.cs
+++ src/Ryujinx.Graphics.GAL/VSyncMode.cs
@@ -1,9 +1,18 @@
-namespace Ryujinx.Graphics.GAL
+
+public enum VSyncMode
 {
-    public enum VSyncMode
+    On,        
+    Off        
+}
+
+
+public static VSyncMode ConvertLegacyMode(LegacyVSyncMode legacyMode)
+{
+    return legacyMode switch
     {
-        Switch,
-        Unbounded,
-        Custom
-    }
+        LegacyVSyncMode.Switch    => VSyncMode.On,     // 自适应视为开启
+        LegacyVSyncMode.Unbounded => VSyncMode.Off,    // 无限制视为关闭
+        LegacyVSyncMode.Custom    => VSyncMode.On,     // 自定义模式默认开启
+        _ => throw new ArgumentOutOfRangeException()
+    };
 }
