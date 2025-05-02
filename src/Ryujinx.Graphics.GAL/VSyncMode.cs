d790f9832ab49cf823c4b0d19068eff295a3a1ac
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-03 03:09:23 +0800

    Update VSyncMode.cs

--- src/Ryujinx.Graphics.GAL/VSyncMode.cs
+++ src/Ryujinx.Graphics.GAL/VSyncMode.cs
@@ -1,25 +1,9 @@
 namespace Ryujinx.Graphics.GAL
 {
-    // 
     public enum VSyncMode
     {
-        On,
-        Off 
-    }
-
-    // 
-    public static class VSyncModeHelper
-    {
-        // 
-        public static VSyncMode ConvertLegacyMode(string configvalue)
-        {
-            return configvalue switch
-            {
-                "Switch"    => VSyncMode.On,
-                "Unbounded" => VSyncMode.Off,
-                "Custom"    => VSyncMode.On,
-                _ => VSyncMode.On 
-            };
-        }
+        Switch,
+        Unbounded,
+        Custom
     }
 }
