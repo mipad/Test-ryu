59ae125c0739c7ddb8a8cdebd8b569b797bd365f
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-06 02:58:00 +0800

    Update TextureManager.cs

--- src/Ryujinx.Graphics.Gpu/Image/TextureManager.cs
+++ src/Ryujinx.Graphics.Gpu/Image/TextureManager.cs
@@ -381,9 +381,9 @@ namespace Ryujinx.Graphics.Gpu.Image
                 texture.FirstLayer,
                 texture.FirstLevel,
                 texture.Samples,          // 新增 samples 参数
-                compressedFormat,         // 压缩后格式
+                compressedFormat         // 压缩后格式
                 //texture.ScaleMode         // 缩放模式
-            )
+            );
         }
     }
 
