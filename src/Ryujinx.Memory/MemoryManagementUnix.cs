b85e6e66284ad46adc27058d86d4c13494e8bf97
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-20 01:39:25 +0800

    Update MemoryManagementUnix.cs

--- src/Ryujinx.Memory/MemoryManagementUnix.cs
+++ src/Ryujinx.Memory/MemoryManagementUnix.cs
@@ -274,32 +274,4 @@ namespace Ryujinx.Memory
         }
     }
 
-    internal static class MemoryManagerUnixHelper
-    {
-        // macOS/Linux系统调用
-        [DllImport("libc", SetLastError = true)]
-        public static extern IntPtr mmap(IntPtr addr, ulong length, MmapProts prot, MmapFlags flags, int fd, long offset);
-        
-        
-    }
-
-    internal enum MmapProts
-    {
-        PROT_NONE = 0x0,
-        PROT_READ = 0x1,
-        PROT_WRITE = 0x2,
-        PROT_EXEC = 0x4
-    }
-
-    [Flags]
-    internal enum MmapFlags
-    {
-        MAP_ANONYMOUS = 0x20,
-        MAP_SHARED = 0x01,
-        MAP_PRIVATE = 0x02,
-        MAP_FIXED = 0x10,
-        MAP_NORESERVE = 0x4000,
-        MAP_JIT_DARWIN = 0x0800,  // macOS JIT标志
-        MAP_UNLOCKED = 0x20000
-    }
-}
+    
