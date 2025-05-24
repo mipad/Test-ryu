43557f5864497689e7e1a077024dd8586ce6f7d1
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-25 00:25:11 +0800

    Update SettingViews.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/SettingViews.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/SettingViews.kt
@@ -1166,6 +1166,7 @@ class SettingViews {
                         enableShaderCache,
                         enableTextureRecompression,
                         resScale,
+                        enableAspectRatioStretch,
                         useVirtualController,
                         isGrid,
                         useSwitchLayout,
