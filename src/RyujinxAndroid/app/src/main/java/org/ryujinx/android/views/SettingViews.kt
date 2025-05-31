278e20bdaf0302aad1ea76b375be8847a6b5af61
shuyi <54129575+mipad@users.noreply.github.com>
2025-05-31 11:32:41 +0800

    Update SettingViews.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/SettingViews.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/SettingViews.kt
@@ -699,8 +699,8 @@ class SettingViews {
                                 Text(text = resScale.value.toString() + "x")
                             }
                             Slider(value = resScale.value,
-                                valueRange = 0.5f..4f,
-                                steps = 6,
+                                valueRange = 0.25f..4f,
+                                steps = 15,
                                 onValueChange = { it ->
                                     resScale.value = it
                                 })
