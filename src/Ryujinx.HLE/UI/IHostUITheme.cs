4c3df5e61e72add859b6c1c68eebbe9154ffa15d
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-08 13:42:33 +0800

    Update IHostUITheme.cs

--- src/Ryujinx.HLE/UI/IHostUITheme.cs
+++ src/Ryujinx.HLE/UI/IHostUITheme.cs
@@ -1,13 +1,17 @@
-namespace Ryujinx.HLE.UI
+public struct ThemeColor
 {
-    public interface IHostUITheme
-    {
-        string FontFamily { get; }
+    public byte R { get; }
+    public byte G { get; }
+    public byte B { get; }
+    public byte A { get; }
 
-        ThemeColor DefaultBackgroundColor { get; }
-        ThemeColor DefaultForegroundColor { get; }
-        ThemeColor DefaultBorderColor { get; }
-        ThemeColor SelectionBackgroundColor { get; }
-        ThemeColor SelectionForegroundColor { get; }
+    public ThemeColor(byte r, byte g, byte b, byte a)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
     }
+    
+    public override string ToString() => $"rgba({R}, {G}, {B}, {A / 255.0:F2})";
 }
