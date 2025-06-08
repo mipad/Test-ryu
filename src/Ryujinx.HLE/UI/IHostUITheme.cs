c331b364ffea8fa3b2c35ad0e0f74c5b34e718d3
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-08 13:51:56 +0800

    Update IHostUITheme.cs

--- src/Ryujinx.HLE/UI/IHostUITheme.cs
+++ src/Ryujinx.HLE/UI/IHostUITheme.cs
@@ -10,24 +10,4 @@ namespace Ryujinx.HLE.UI
         ThemeColor SelectionBackgroundColor { get; }
         ThemeColor SelectionForegroundColor { get; }
     }
-    
-    // 新增：ThemeColor 结构体实现
-    public struct ThemeColor
-    {
-        public byte R { get; }
-        public byte G { get; }
-        public byte B { get; }
-        public byte A { get; }
-
-        public ThemeColor(byte r, byte g, byte b, byte a)
-        {
-            R = r;
-            G = g;
-            B = b;
-            A = a;
-        }
-        
-        // 转换为RGBA格式字符串
-        public override string ToString() => $"rgba({R}, {G}, {B}, {A / 255.0:F2})";
-    }
 }
