adcf6926fa7d4866bbe5e91b98b57006cfb7e69c
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-11 18:59:39 +0800

    Update VkException.cs

--- src/Ryujinx.Graphics.Vulkan/VkException.cs
+++ src/Ryujinx.Graphics.Vulkan/VkException.cs
@@ -1,18 +1,18 @@
 using System;
-using Silk.NET.Vulkan; 
+using Silk.NET.Vulkan; // 根据实际使用的库
 
 namespace Ryujinx.Graphics.Vulkan
 {
     public class VkException : Exception
     {
+        public Result Result { get; }
+
         public VkException(Result result) 
-            : base($"Vulkan API error: {result}")
-        {
-        }
+            : base($"Vulkan API error: {result}") 
+            => Result = result;
 
         public VkException(Result result, string message) 
-            : base($"Vulkan error ({result}): {message}")
-        {
-        }
+            : base($"Vulkan error ({result}): {message}") 
+            => Result = result;
     }
 }
