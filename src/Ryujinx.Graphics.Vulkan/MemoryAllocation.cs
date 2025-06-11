192f5058a90564dfbd46a6a1df0763d25b99dd6a
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-11 09:51:17 +0800

    Update MemoryAllocation.cs

--- src/Ryujinx.Graphics.Vulkan/MemoryAllocation.cs
+++ src/Ryujinx.Graphics.Vulkan/MemoryAllocation.cs
@@ -13,9 +13,6 @@ namespace Ryujinx.Graphics.Vulkan
         public IntPtr HostPointer { get; }
         public ulong Offset { get; }
         public ulong Size { get; }
-        
-        // 新增：检查分配是否有效的属性
-        public bool IsValid => Memory.Handle != 0;
 
         public MemoryAllocation(
             MemoryAllocatorBlockList owner,
@@ -55,7 +52,7 @@ namespace Ryujinx.Graphics.Vulkan
             }
             else
             {
-                _owner?.Free(_block, Offset, Size);
+                _owner.Free(_block, Offset, Size);
             }
         }
     }
