04a6f0696b49bcd935dc05877c48f7f7a020c662
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-13 06:23:03 +0800

    Update MemoryManager.cs

--- src/Ryujinx.Cpu/Jit/MemoryManager.cs
+++ src/Ryujinx.Cpu/Jit/MemoryManager.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
+using Ryujinx.Common.Logging;
 
 namespace Ryujinx.Cpu.Jit
 {
