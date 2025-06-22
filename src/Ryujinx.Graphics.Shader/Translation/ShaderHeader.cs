dd68c4601f99a8a147ed0c0bab313407b385329c
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-23 01:13:19 +0800

    Update ShaderHeader.cs

--- src/Ryujinx.Graphics.Shader/Translation/ShaderHeader.cs
+++ src/Ryujinx.Graphics.Shader/Translation/ShaderHeader.cs
@@ -61,7 +61,7 @@ namespace Ryujinx.Graphics.Shader.Translation
 
         public int SassVersion { get; }
 
-        public bool GpPassthrough { get; private set; }
+        public bool GpPassthrough { get; internal set; } // 修改为internal set
 
         public bool DoesLoadOrStore { get; }
         public bool DoesFp64 { get; }
@@ -163,11 +163,6 @@ namespace Ryujinx.Graphics.Shader.Translation
             OmapTargets = type2OmapTarget;
             OmapSampleMask = type2Omap.Extract(0);
             OmapDepth = type2Omap.Extract(1);
-        }
-
-        public void SetGpPassthrough(bool value)
-        {
-            this.GpPassthrough = value;
         }
     }
 }
