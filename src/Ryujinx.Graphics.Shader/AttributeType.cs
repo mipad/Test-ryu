b68afbea9aab5ceadb16b83676fd4dbe8cd65809
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-24 22:37:10 +0800

    Update AttributeType.cs

--- src/Ryujinx.Graphics.Shader/AttributeType.cs
+++ src/Ryujinx.Graphics.Shader/AttributeType.cs
@@ -22,26 +22,46 @@ namespace Ryujinx.Graphics.Shader
     {
         public static AggregateType ToAggregateType(this AttributeType type)
         {
-            return (type & ~AttributeType.AnyPacked) switch
+            // 安全处理Invalid类型
+            if (type == AttributeType.Invalid)
+            {
+                return AggregateType.FP32;
+            }
+            
+            var baseType = type & ~AttributeType.AnyPacked;
+            return baseType switch
             {
                 AttributeType.Float => AggregateType.FP32,
                 AttributeType.Sint => AggregateType.S32,
                 AttributeType.Uint => AggregateType.U32,
-                _ => throw new ArgumentException($"Invalid attribute type \"{type}\"."),
+                _ => AggregateType.FP32 // 默认回退
             };
         }
 
         public static AggregateType ToAggregateType(this AttributeType type, bool supportsScaledFormats)
         {
-            return (type & ~AttributeType.AnyPacked) switch
+            // 安全处理Invalid类型
+            if (type == AttributeType.Invalid)
+            {
+                return AggregateType.FP32;
+            }
+            
+            var baseType = type & ~AttributeType.AnyPacked;
+            return baseType switch
             {
                 AttributeType.Float => AggregateType.FP32,
                 AttributeType.Sint => AggregateType.S32,
                 AttributeType.Uint => AggregateType.U32,
                 AttributeType.Sscaled => supportsScaledFormats ? AggregateType.FP32 : AggregateType.S32,
                 AttributeType.Uscaled => supportsScaledFormats ? AggregateType.FP32 : AggregateType.U32,
-                _ => throw new ArgumentException($"Invalid attribute type \"{type}\"."),
+                _ => AggregateType.FP32 // 默认回退
             };
+        }
+
+        // 新增安全转换方法
+        public static AttributeType Sanitize(this AttributeType type)
+        {
+            return type == AttributeType.Invalid ? AttributeType.Float : type;
         }
     }
 }
