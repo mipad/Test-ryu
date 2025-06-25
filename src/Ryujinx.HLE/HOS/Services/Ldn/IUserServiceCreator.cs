3f977be87a91f94d284de153fa791119d21c1323
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-25 14:50:38 +0800

    Update IUserServiceCreator.cs

--- src/Ryujinx.HLE/HOS/Services/Ldn/IUserServiceCreator.cs
+++ src/Ryujinx.HLE/HOS/Services/Ldn/IUserServiceCreator.cs
@@ -8,11 +8,11 @@ namespace Ryujinx.HLE.HOS.Services.Ldn
         public IUserServiceCreator(ServiceCtx context) : base(context.Device.System.LdnServer) { }
 
         [CommandCmif(0)]
-        // CreateUserLocalCommunicationService() -> object<nn::ldn::detail::IUserLocalCommunicationService>
         public ResultCode CreateUserLocalCommunicationService(ServiceCtx context)
         {
+            // +++ 允许创建服务但内部模拟无网络状态 +++
             MakeObject(context, new IUserLocalCommunicationService(context));
-
+            Logger.Info?.Print(LogClass.ServiceLdn, "Network service created (simulated disabled state)");
             return ResultCode.Success;
         }
     }
