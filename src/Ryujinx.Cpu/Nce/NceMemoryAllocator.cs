58d8011580f699d4ae093ccc04b00245ca2b932f
shuyi <54129575+mipad@users.noreply.github.com>
2025-06-28 23:26:17 +0800

    Update NceMemoryAllocator.cs

--- src/Ryujinx.Cpu/Nce/NceMemoryAllocator.cs
+++ src/Ryujinx.Cpu/Nce/NceMemoryAllocator.cs
@@ -1,8 +1,13 @@
-﻿using ARMeilleure.Memory;
+using ARMeilleure.Memory;
 using Ryujinx.Memory;
+using System.Runtime.Versioning;
 
 namespace Ryujinx.Cpu.Nce
 {
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("android")]
+    [SupportedOSPlatform("windows")]
     class NceMemoryAllocator : IJitMemoryAllocator
     {
         public IJitMemoryBlock Allocate(ulong size) => new NceMemoryBlock(size, MemoryAllocationFlags.None);
