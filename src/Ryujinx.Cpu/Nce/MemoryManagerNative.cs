d937caefdb52132def265751f9f93cc8f779a253
shuyi <54129575+mipad@users.noreply.github.com>
2025-07-10 19:25:02 +0800

    Update MemoryManagerNative.cs

--- src/Ryujinx.Cpu/Nce/MemoryManagerNative.cs
+++ src/Ryujinx.Cpu/Nce/MemoryManagerNative.cs
@@ -1,4 +1,4 @@
-﻿using ARMeilleure.Memory;
+using ARMeilleure.Memory;
 using Ryujinx.Memory;
 using Ryujinx.Memory.Range;
 using Ryujinx.Memory.Tracking;
@@ -151,6 +151,22 @@ namespace Ryujinx.Cpu.Nce
         public bool IsRangeMapped(ulong va, ulong size)
         {
             AssertValidAddressAndSize(va, size);
+
+            return _pages.IsRangeMapped(va, size);
+        }
+
+        /// <inheritdoc/>
+        public bool IsRangeMappedSafe(ulong va, ulong size)
+        {
+            if (size == 0UL)
+            {
+                return true;
+            }
+
+            if (!ValidateAddressAndSize(va, size))
+            {
+                return false;
+            }
 
             return _pages.IsRangeMapped(va, size);
         }
