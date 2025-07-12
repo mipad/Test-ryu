98996cf3619b492193d539709658103a14a6f2f3
shuyi <54129575+mipad@users.noreply.github.com>
2025-07-12 20:00:19 +0800

    Update IHardwareDeviceSession.cs

--- src/Ryujinx.Audio/Integration/IHardwareDeviceSession.cs
+++ src/Ryujinx.Audio/Integration/IHardwareDeviceSession.cs
@@ -1,28 +1,98 @@
-using Ryujinx.Audio.Common;
-using System;
+using System.Collections.Generic;
 
 namespace Ryujinx.Audio.Integration
 {
-    public interface IHardwareDeviceSession : IDisposable
+    /// <summary>
+    /// Represent a hardware device session on the device driver.
+    /// </summary>
+    public interface IHardwareDeviceSession
     {
+        /// <summary>
+        /// The direction of the session.
+        /// </summary>
+        Direction Direction { get; }
+
+        /// <summary>
+        /// Register a new buffer.
+        /// </summary>
+        /// <param name="buffer">The buffer to register</param>
+        /// <returns>True if the registration was successful</returns>
         bool RegisterBuffer(AudioBuffer buffer);
 
+        /// <summary>
+        /// Unregister a buffer.
+        /// </summary>
+        /// <param name="buffer">The buffer to unregister</param>
         void UnregisterBuffer(AudioBuffer buffer);
 
+        /// <summary>
+        /// Queue a buffer for playing.
+        /// </summary>
+        /// <param name="buffer">The buffer to queue</param>
         void QueueBuffer(AudioBuffer buffer);
 
+        /// <summary>
+        /// Batch queue buffers for playing.
+        /// </summary>
+        /// <param name="buffers">The list of buffers to queue</param>
+        void QueueBuffers(IList<AudioBuffer> buffers);
+
+        /// <summary>
+        /// Get released buffers from hardware.
+        /// </summary>
+        /// <param name="maxCount">Maximum number of buffers to retrieve</param>
+        /// <returns>List of released buffers</returns>
+        IList<AudioBuffer> GetReleasedBuffers(int maxCount);
+
+        /// <summary>
+        /// Check if a buffer has been fully consumed.
+        /// </summary>
+        /// <param name="buffer">The buffer to check</param>
+        /// <returns>True if the buffer has been fully consumed</returns>
         bool WasBufferFullyConsumed(AudioBuffer buffer);
 
-        void SetVolume(float volume);
-
-        float GetVolume();
-
-        ulong GetPlayedSampleCount();
-
+        /// <summary>
+        /// Start the session.
+        /// </summary>
         void Start();
 
+        /// <summary>
+        /// Stop the session.
+        /// </summary>
         void Stop();
 
+        /// <summary>
+        /// Prepare the session for closing.
+        /// </summary>
         void PrepareToClose();
+
+        /// <summary>
+        /// Get the volume of the session.
+        /// </summary>
+        /// <returns>The volume of the session</returns>
+        float GetVolume();
+
+        /// <summary>
+        /// Set the volume of the session.
+        /// </summary>
+        /// <param name="volume">The new volume to set</param>
+        void SetVolume(float volume);
+
+        /// <summary>
+        /// Get the played sample count.
+        /// </summary>
+        /// <returns>The played sample count</returns>
+        ulong GetPlayedSampleCount();
+
+        /// <summary>
+        /// Create a pre-encoded silence buffer.
+        /// </summary>
+        /// <returns>A silence buffer in the correct format</returns>
+        AudioBuffer CreateSilenceBuffer();
+
+        /// <summary>
+        /// Dispose the session.
+        /// </summary>
+        void Dispose();
     }
 }
