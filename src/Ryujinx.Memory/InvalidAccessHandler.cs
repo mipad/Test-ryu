37acb970022a9a1417d8d2a0e1f99198a0b5cdec
shuyi <54129575+mipad@users.noreply.github.com>
2025-07-19 19:53:38 +0800

    Update InvalidAccessHandler.cs

--- src/Ryujinx.Memory/InvalidAccessHandler.cs
+++ src/Ryujinx.Memory/InvalidAccessHandler.cs
@@ -4,6 +4,8 @@ namespace Ryujinx.Memory
     /// Function that handles a invalid memory access from the emulated CPU.
     /// </summary>
     /// <param name="va">Virtual address of the invalid region that is being accessed</param>
+    /// <param name="size">Size of the access attempt</param>
+    /// <param name="write">Whether the access was a write operation</param>
     /// <returns>True if the invalid access should be ignored, false otherwise</returns>
-    public delegate bool InvalidAccessHandler(ulong va);
+    public delegate bool InvalidAccessHandler(ulong va, ulong size, bool write);
 }
