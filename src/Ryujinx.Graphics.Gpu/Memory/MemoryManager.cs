f64f51530833de3b46395703e4f43ea106f291dc
shuyi <54129575+mipad@users.noreply.github.com>
2025-07-27 00:34:27 +0800

    Update MemoryManager.cs

--- src/Ryujinx.Graphics.Gpu/Memory/MemoryManager.cs
+++ src/Ryujinx.Graphics.Gpu/Memory/MemoryManager.cs
@@ -706,10 +706,7 @@ namespace Ryujinx.Graphics.Gpu.Memory
             {
                 _pageTable[l0] = new ulong[PtLvl1Size];
 
-                for (ulong index = 0; index < PtLvl1Size; index++)
-                {
-                    _pageTable[l0][index] = PteUnmapped;
-                }
+                Array.Fill(_pageTable[l0], PteUnmapped);
             }
 
             _pageTable[l0][l1] = pte;
