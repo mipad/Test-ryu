df603970d31a1a6c4477073bd03e4df2b5a49e75
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-04 11:41:04 +0800

    Update PartitionFileSystemUtils.cs

--- src/Ryujinx.HLE/Utilities/PartitionFileSystemUtils.cs
+++ src/Ryujinx.HLE/Utilities/PartitionFileSystemUtils.cs
@@ -10,11 +10,24 @@ namespace Ryujinx.HLE.Utilities
 {
     public static class PartitionFileSystemUtils
     {
-        public static IFileSystem OpenApplicationFileSystem(string path, VirtualFileSystem fileSystem, bool throwOnFailure = true)
+        public static IFileSystem OpenApplicationFileSystem(string path, bool isXci, VirtualFileSystem fileSystem, bool throwOnFailure = true)
         {
-            FileStream file = File.OpenRead(path);
+            // 不要在这里创建 FileStream，而是在内部方法中处理
+            return OpenApplicationFileSystem(path, isXci, fileSystem, throwOnFailure, true);
+        }
 
-            return OpenApplicationFileSystem(file, Path.GetExtension(path).ToLower() == ".xci", fileSystem, throwOnFailure);
+        private static IFileSystem OpenApplicationFileSystem(string path, bool isXci, VirtualFileSystem fileSystem, bool throwOnFailure, bool openFile)
+        {
+            if (openFile)
+            {
+                using FileStream file = File.OpenRead(path);
+                return OpenApplicationFileSystem(file, isXci, fileSystem, throwOnFailure);
+            }
+            else
+            {
+                // 这个路径不应该被调用，只是为了保持方法签名一致
+                return null;
+            }
         }
 
         public static IFileSystem OpenApplicationFileSystem(Stream stream, bool isXci, VirtualFileSystem fileSystem, bool throwOnFailure = true)
@@ -23,7 +36,9 @@ namespace Ryujinx.HLE.Utilities
 
             if (isXci)
             {
-                partitionFileSystem = new Xci(fileSystem.KeySet, stream.AsStorage()).OpenPartition(XciPartitionType.Secure);
+                // 创建 Xci 对象，但不让它在析构时关闭流
+                var xci = new Xci(fileSystem.KeySet, stream.AsStorage());
+                partitionFileSystem = xci.OpenPartition(XciPartitionType.Secure);
             }
             else
             {
