f1a6a0148df003e9cec5f8b84ab20163eeaf2e07
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-12 01:53:55 +0800

    Update TickSource.cs

--- src/Ryujinx.Cpu/TickSource.cs
+++ src/Ryujinx.Cpu/TickSource.cs
@@ -5,9 +5,12 @@ namespace Ryujinx.Cpu
 {
     public class TickSource : ITickSource
     {
-        private static Stopwatch _tickCounter;
+        private Stopwatch _tickCounter;
+        private double _hostTickFreq;
 
-        private static double _hostTickFreq;
+        private long _tickScalar = 100; // 默认 100%
+        private long _acumElapsedTicks;
+        private long _lastElapsedTicks;
 
         /// <inheritdoc/>
         public ulong Frequency { get; }
@@ -15,11 +18,31 @@ namespace Ryujinx.Cpu
         /// <inheritdoc/>
         public ulong Counter => (ulong)(ElapsedSeconds * Frequency);
 
+        public long TickScalar
+        {
+            get => _tickScalar;
+            set => _tickScalar = Math.Clamp(value, 0, 400); // 限制在 0-400%
+        }
+
+        private long ElapsedTicks
+        {
+            get
+            {
+                long elapsedTicks = _tickCounter.ElapsedTicks;
+                
+                _acumElapsedTicks += (elapsedTicks - _lastElapsedTicks) * _tickScalar / 100;
+
+                _lastElapsedTicks = elapsedTicks;
+                
+                return _acumElapsedTicks;
+            }
+        }
+        
         /// <inheritdoc/>
-        public TimeSpan ElapsedTime => _tickCounter.Elapsed;
+        public TimeSpan ElapsedTime => Stopwatch.GetElapsedTime(0, ElapsedTicks);
 
         /// <inheritdoc/>
-        public double ElapsedSeconds => _tickCounter.ElapsedTicks * _hostTickFreq;
+        public double ElapsedSeconds => ElapsedTicks * _hostTickFreq;
 
         public TickSource(ulong frequency)
         {
