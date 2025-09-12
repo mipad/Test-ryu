dbea0570fb8f02db33bd07db14b77472fabb4258
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-13 04:34:17 +0800

    Update SurfaceFlinger.cs

--- src/Ryujinx.HLE/HOS/Services/SurfaceFlinger/SurfaceFlinger.cs
+++ src/Ryujinx.HLE/HOS/Services/SurfaceFlinger/SurfaceFlinger.cs
@@ -520,7 +520,7 @@ namespace Ryujinx.HLE.HOS.Services.SurfaceFlinger
                 frameBufferAddress,
                 frameBufferWidth,
                 frameBufferHeight,
-                item.Transform,
+                (int)item.Transform,
                 false,
                 gobBlocksInY,
                 format,
