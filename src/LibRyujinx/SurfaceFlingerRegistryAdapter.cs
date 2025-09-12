b3dffc0f08ebf9b6c1d1ccbd804833f373373a50
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-12 23:14:41 +0800

    Update SurfaceFlingerRegistryAdapter.cs

--- src/LibRyujinx/SurfaceFlingerRegistryAdapter.cs
+++ src/LibRyujinx/SurfaceFlingerRegistryAdapter.cs
@@ -1,17 +1,28 @@
 using Ryujinx.Core;
+using System;
 
 namespace LibRyujinx
 {
     public class SurfaceFlingerRegistryAdapter : ISurfaceFlingerRegistry
     {
-        public void SetSurfaceFlingerInstance(object surfaceFlinger)
+        private Action _targetFpsUpdateCallback;
+        
+        public void RegisterTargetFpsUpdateCallback(Action callback)
         {
-            LibRyujinx.SetSurfaceFlingerInstance(surfaceFlinger as SurfaceFlinger);
+            _targetFpsUpdateCallback = callback;
+        }
+
+        public void SetSurfaceFlingerInstance(ISurfaceFlinger surfaceFlinger)
+        {
+            // 通过接口而不是具体类来传递实例
+            // 这里假设 LibRyujinx 有一个接受 ISurfaceFlinger 的方法
+            LibRyujinx.SetSurfaceFlingerInstance(surfaceFlinger);
         }
 
         public void UpdateSurfaceFlingerTargetFps()
         {
-            LibRyujinx.UpdateSurfaceFlingerTargetFps();
+            // 调用注册的回调，而不是直接调用 LibRyujinx 的方法
+            _targetFpsUpdateCallback?.Invoke();
         }
     }
 }
