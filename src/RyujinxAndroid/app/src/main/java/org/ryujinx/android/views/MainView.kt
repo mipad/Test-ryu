0c7f28b7e23fa2085398bf8b0f7e0a92d9935ba4
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-13 23:00:40 +0800

    Update MainView.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/MainView.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/MainView.kt
@@ -1,9 +1,11 @@
 package org.ryujinx.android.views
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import org.ryujinx.android.viewmodels.MainViewModel
 import org.ryujinx.android.viewmodels.SettingsViewModel
 
@@ -25,6 +27,17 @@ class MainView {
                             mainViewModel.activity
                         ), mainViewModel
                     )
+                }
+                // 添加金手指界面导航
+                composable(
+                    "cheats/{titleId}",
+                    arguments = listOf(
+                        navArgument("titleId") { type = NavType.StringType }
+                    )
+                ) { backStackEntry ->
+                    val titleId = backStackEntry.arguments?.getString("titleId") ?: ""
+                    val gamePath = backStackEntry.arguments?.getString("gamePath") ?: ""
+                    CheatsViews(navController, titleId, gamePath)
                 }
             }
         }
