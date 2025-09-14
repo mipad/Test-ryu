8ff5e9611ab636219b8668bf0bc1600421142ecd
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-15 00:38:38 +0800

    Update MemoryHelper.cs

--- src/Ryujinx.HLE/HOS/Tamper/MemoryHelper.cs
+++ src/Ryujinx.HLE/HOS/Tamper/MemoryHelper.cs
@@ -1,3 +1,4 @@
+using Ryujinx.Common.Logging;
 using Ryujinx.HLE.Exceptions;
 using Ryujinx.HLE.HOS.Tamper.Operations;
 
@@ -7,39 +8,54 @@ namespace Ryujinx.HLE.HOS.Tamper
     {
         public static ulong GetAddressShift(MemoryRegion source, CompilationContext context)
         {
-            return source switch
+            ulong addressShift = source switch
             {
-                // Memory address is relative to the code start.
                 MemoryRegion.NSO => context.ExeAddress,
-                // Memory address is relative to the heap.
                 MemoryRegion.Heap => context.HeapAddress,
-                // Memory address is relative to the alias region.
                 MemoryRegion.Alias => context.AliasAddress,
-                // Memory address is relative to the asrl region, which matches the code region.
                 MemoryRegion.Asrl => context.AslrAddress,
                 _ => throw new TamperCompilationException($"Invalid memory source {source} in Atmosphere cheat"),
             };
+            
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"GetAddressShift: region={source}, address=0x{addressShift:X16}");
+            
+            return addressShift;
         }
 
         private static void EmitAdd(Value<ulong> finalValue, IOperand firstOperand, IOperand secondOperand, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitAdd: first=0x{firstOperand.Get<ulong>():X16}, second=0x{secondOperand.Get<ulong>():X16}");
+            
             context.CurrentOperations.Add(new OpAdd<ulong>(finalValue, firstOperand, secondOperand));
+            
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitAdd: result=0x{finalValue.Get<ulong>():X16}");
         }
 
         public static Pointer EmitPointer(ulong addressImmediate, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: immediate=0x{addressImmediate:X16}");
+            
             Value<ulong> addressImmediateValue = new(addressImmediate);
-
             return new Pointer(addressImmediateValue, context.Process);
         }
 
         public static Pointer EmitPointer(Register addressRegister, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: register={addressRegister}, value=0x{addressRegister.Get<ulong>():X16}");
+            
             return new Pointer(addressRegister, context.Process);
         }
 
         public static Pointer EmitPointer(Register addressRegister, ulong offsetImmediate, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: register={addressRegister}, offset=0x{offsetImmediate:X16}");
+            
             Value<ulong> offsetImmediateValue = new(offsetImmediate);
             Value<ulong> finalAddressValue = new(0);
             EmitAdd(finalAddressValue, addressRegister, offsetImmediateValue, context);
@@ -49,6 +65,9 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         public static Pointer EmitPointer(Register addressRegister, Register offsetRegister, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: addressReg={addressRegister}, offsetReg={offsetRegister}");
+            
             Value<ulong> finalAddressValue = new(0);
             EmitAdd(finalAddressValue, addressRegister, offsetRegister, context);
 
@@ -57,6 +76,9 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         public static Pointer EmitPointer(Register addressRegister, Register offsetRegister, ulong offsetImmediate, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: addressReg={addressRegister}, offsetReg={offsetRegister}, offsetImm=0x{offsetImmediate:X16}");
+            
             Value<ulong> offsetImmediateValue = new(offsetImmediate);
             Value<ulong> finalOffsetValue = new(0);
             EmitAdd(finalOffsetValue, offsetRegister, offsetImmediateValue, context);
@@ -68,22 +90,39 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         public static Pointer EmitPointer(MemoryRegion memoryRegion, ulong offsetImmediate, CompilationContext context)
         {
-            offsetImmediate += GetAddressShift(memoryRegion, context);
-
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: region={memoryRegion}, offset=0x{offsetImmediate:X16}");
+            
+            ulong baseAddress = GetAddressShift(memoryRegion, context);
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: baseAddress=0x{baseAddress:X16}, finalAddress=0x{baseAddress + offsetImmediate:X16}");
+            
+            offsetImmediate += baseAddress;
             return EmitPointer(offsetImmediate, context);
         }
 
         public static Pointer EmitPointer(MemoryRegion memoryRegion, Register offsetRegister, CompilationContext context)
         {
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: region={memoryRegion}, offsetReg={offsetRegister}");
+            
             ulong offsetImmediate = GetAddressShift(memoryRegion, context);
-
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: baseAddress=0x{offsetImmediate:X16}");
+            
             return EmitPointer(offsetRegister, offsetImmediate, context);
         }
 
         public static Pointer EmitPointer(MemoryRegion memoryRegion, Register offsetRegister, ulong offsetImmediate, CompilationContext context)
         {
-            offsetImmediate += GetAddressShift(memoryRegion, context);
-
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: region={memoryRegion}, offsetReg={offsetRegister}, offsetImm=0x{offsetImmediate:X16}");
+            
+            ulong baseAddress = GetAddressShift(memoryRegion, context);
+            Logger.Debug?.Print(LogClass.TamperMachine, 
+                $"EmitPointer: baseAddress=0x{baseAddress:X16}, finalAddress=0x{baseAddress + offsetImmediate:X16}");
+            
+            offsetImmediate += baseAddress;
             return EmitPointer(offsetRegister, offsetImmediate, context);
         }
     }
