a9b688c198609f81d48b8ddeb06b82c5701eb518
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-14 16:10:43 +0800

    Update Register.cs

--- src/Ryujinx.HLE/HOS/Tamper/Register.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Register.cs
@@ -1,5 +1,6 @@
 using Ryujinx.Common.Logging;
 using Ryujinx.HLE.HOS.Tamper.Operations;
+using System;
 
 namespace Ryujinx.HLE.HOS.Tamper
 {
@@ -15,14 +16,34 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         public T Get<T>() where T : unmanaged
         {
-            return (T)(dynamic)_register;
+            // 避免使用动态类型转换
+            if (typeof(T) == typeof(byte))
+                return (T)(object)(byte)_register;
+            else if (typeof(T) == typeof(ushort))
+                return (T)(object)(ushort)_register;
+            else if (typeof(T) == typeof(uint))
+                return (T)(object)(uint)_register;
+            else if (typeof(T) == typeof(ulong))
+                return (T)(object)_register;
+            else
+                throw new NotSupportedException($"Type {typeof(T)} is not supported in Register.Get");
         }
 
         public void Set<T>(T value) where T : unmanaged
         {
             Logger.Debug?.Print(LogClass.TamperMachine, $"{_alias}: {value}");
 
-            _register = (ulong)(dynamic)value;
+            // 避免使用动态类型转换
+            if (typeof(T) == typeof(byte))
+                _register = (byte)(object)value;
+            else if (typeof(T) == typeof(ushort))
+                _register = (ushort)(object)value;
+            else if (typeof(T) == typeof(uint))
+                _register = (uint)(object)value;
+            else if (typeof(T) == typeof(ulong))
+                _register = (ulong)(object)value;
+            else
+                throw new NotSupportedException($"Type {typeof(T)} is not supported in Register.Set");
         }
     }
 }
