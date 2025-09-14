4c81bfef8009c5e08cb22eba719442ca31171fef
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-14 16:55:01 +0800

    Update OpMov.cs

--- src/Ryujinx.HLE/HOS/Tamper/Operations/OpMov.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Operations/OpMov.cs
@@ -1,5 +1,3 @@
-using Ryujinx.Common.Logging;
-
 namespace Ryujinx.HLE.HOS.Tamper.Operations
 {
     class OpMov<T> : IOperation where T : unmanaged
@@ -7,22 +5,14 @@ namespace Ryujinx.HLE.HOS.Tamper.Operations
         readonly IOperand _destination;
         readonly IOperand _source;
 
-        // 添加无参数构造函数
-        public OpMov()
-        {
-            Logger.Debug?.Print(LogClass.TamperMachine, $"OpMov<{typeof(T).Name}> no-arg constructor called");
-        }
-
         public OpMov(IOperand destination, IOperand source)
         {
-            Logger.Debug?.Print(LogClass.TamperMachine, $"OpMov<{typeof(T).Name}> two-arg constructor called with {destination} and {source}");
             _destination = destination;
             _source = source;
         }
 
         public void Execute()
         {
-            Logger.Debug?.Print(LogClass.TamperMachine, $"OpMov<{typeof(T).Name}> executing");
             _destination.Set(_source.Get<T>());
         }
     }
