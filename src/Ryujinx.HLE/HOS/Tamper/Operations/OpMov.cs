60a07f30b8ac397eae76d67198cf665aedaf9a27
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-15 10:04:35 +0800

    Update OpMov.cs

--- src/Ryujinx.HLE/HOS/Tamper/Operations/OpMov.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Operations/OpMov.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Ryujinx.HLE.HOS.Tamper.Operations
 {
     class OpMov<T> : IOperation where T : unmanaged
@@ -13,7 +15,31 @@ namespace Ryujinx.HLE.HOS.Tamper.Operations
 
         public void Execute()
         {
-            _destination.Set(_source.Get<T>());
+            // 使用显式类型转换而不是依赖 dynamic
+            if (typeof(T) == typeof(byte))
+            {
+                byte value = _source.Get<byte>();
+                _destination.Set(value);
+            }
+            else if (typeof(T) == typeof(ushort))
+            {
+                ushort value = _source.Get<ushort>();
+                _destination.Set(value);
+            }
+            else if (typeof(T) == typeof(uint))
+            {
+                uint value = _source.Get<uint>();
+                _destination.Set(value);
+            }
+            else if (typeof(T) == typeof(ulong))
+            {
+                ulong value = _source.Get<ulong>();
+                _destination.Set(value);
+            }
+            else
+            {
+                throw new NotSupportedException($"Type {typeof(T)} is not supported for MOV operation");
+            }
         }
     }
 }
