4d1871c9f102a57d0bc95f3e9903cf1846a22dc2
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-15 20:07:28 +0800

    Update Pointer.cs

--- src/Ryujinx.HLE/HOS/Tamper/Pointer.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Pointer.cs
@@ -1,47 +1,32 @@
 using Ryujinx.Common.Logging;
 using Ryujinx.HLE.HOS.Tamper.Operations;
-using System;
+using System.Runtime.CompilerServices;
 
 namespace Ryujinx.HLE.HOS.Tamper
 {
     class Pointer : IOperand
     {
-        private readonly IOperand _address;
+        private readonly IOperand _position;
         private readonly ITamperedProcess _process;
 
-        public Pointer(IOperand address, ITamperedProcess process)
+        public Pointer(IOperand position, ITamperedProcess process)
         {
-            _address = address;
+            _position = position;
             _process = process;
         }
 
         public T Get<T>() where T : unmanaged
         {
-            ulong address = _address.Get<ulong>();
-            T value = _process.ReadMemory<T>(address);
-            
-            Logger.Debug?.Print(LogClass.TamperMachine, 
-                $"Pointer.Get: Read 0x{value:X} from address 0x{address:X16}");
-            
-            return value;
+            return _process.ReadMemory<T>(_position.Get<ulong>());
         }
 
         public void Set<T>(T value) where T : unmanaged
         {
-            ulong address = _address.Get<ulong>();
-            
-            Logger.Debug?.Print(LogClass.TamperMachine, 
-                $"Pointer.Set: Writing 0x{value:X} to address 0x{address:X16}");
-            
-            _process.WriteMemory(address, value);
-            
-            Logger.Debug?.Print(LogClass.TamperMachine, 
-                $"Pointer.Set: Successfully wrote 0x{value:X} to address 0x{address:X16}");
-        }
+            ulong position = _position.Get<ulong>();
 
-        public IOperand GetPositionOperand()
-        {
-            return _address;
+            Logger.Debug?.Print(LogClass.TamperMachine, $"0x{position:X16}@{Unsafe.SizeOf<T>()}: {value:X}");
+
+            _process.WriteMemory(position, value);
         }
     }
 }
