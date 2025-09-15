4ddb977494ca078da246fec61ac3c31064de352e
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-15 19:32:07 +0800

    Update Value.cs

--- src/Ryujinx.HLE/HOS/Tamper/Value.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Value.cs
@@ -1,5 +1,5 @@
+using Ryujinx.HLE.HOS.Tamper.Operations;
 using System;
-using System.Runtime.CompilerServices;
 
 namespace Ryujinx.HLE.HOS.Tamper
 {
@@ -14,52 +14,80 @@ namespace Ryujinx.HLE.HOS.Tamper
 
         public T Get<T>() where T : unmanaged
         {
-            // 使用 Unsafe 类进行高效且类型安全的转换
-            if (typeof(T) == typeof(TP))
-            {
-                return Unsafe.As<TP, T>(ref _value);
-            }
-            
-            // 对于不同类型之间的转换，使用标准的转换方法
-            return ConvertValue<T>(_value);
+            // 避免使用动态类型转换
+            if (typeof(T) == typeof(byte) && typeof(TP) == typeof(byte))
+                return (T)(object)_value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(ushort))
+                return (T)(object)_value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(uint))
+                return (T)(object)_value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(ulong))
+                return (T)(object)_value;
+            else if (typeof(T) == typeof(byte) && typeof(TP) == typeof(ushort))
+                return (T)(object)(byte)(ushort)(object)_value;
+            else if (typeof(T) == typeof(byte) && typeof(TP) == typeof(uint))
+                return (T)(object)(byte)(uint)(object)_value;
+            else if (typeof(T) == typeof(byte) && typeof(TP) == typeof(ulong))
+                return (T)(object)(byte)(ulong)(object)_value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(byte))
+                return (T)(object)(ushort)(byte)(object)_value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(uint))
+                return (T)(object)(ushort)(uint)(object)_value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(ulong))
+                return (T)(object)(ushort)(ulong)(object)_value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(byte))
+                return (T)(object)(uint)(byte)(object)_value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(ushort))
+                return (T)(object)(uint)(ushort)(object)_value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(ulong))
+                return (T)(object)(uint)(ulong)(object)_value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(byte))
+                return (T)(object)(ulong)(byte)(object)_value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(ushort))
+                return (T)(object)(ulong)(ushort)(object)_value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(uint))
+                return (T)(object)(ulong)(uint)(object)_value;
+            else
+                throw new NotSupportedException($"Conversion from {typeof(TP)} to {typeof(T)} is not supported in Value.Get");
         }
 
         public void Set<T>(T value) where T : unmanaged
         {
-            // 使用 Unsafe 类进行高效且类型安全的转换
-            if (typeof(T) == typeof(TP))
-            {
-                _value = Unsafe.As<T, TP>(ref value);
-                return;
-            }
-            
-            // 对于不同类型之间的转换，使用标准的转换方法
-            _value = ConvertValue<TP>(value);
-        }
-
-        private static TTarget ConvertValue<TSource, TTarget>(TSource value)
-            where TSource : unmanaged
-            where TTarget : unmanaged
-        {
-            // 实现标准的值转换逻辑
-            // 可以根据需要添加具体的转换规则
-            if (typeof(TTarget) == typeof(byte)) return (TTarget)(object)Convert.ToByte(value);
-            if (typeof(TTarget) == typeof(ushort)) return (TTarget)(object)Convert.ToUInt16(value);
-            if (typeof(TTarget) == typeof(uint)) return (TTarget)(object)Convert.ToUInt32(value);
-            if (typeof(TTarget) == typeof(ulong)) return (TTarget)(object)Convert.ToUInt64(value);
-            
-            throw new NotSupportedException($"Conversion from {typeof(TSource)} to {typeof(TTarget)} is not supported");
-        }
-
-        // 重载方法以便于使用
-        private static T ConvertValue<T>(object value) where T : unmanaged
-        {
-            if (value is byte b) return (T)Convert.ChangeType(b, typeof(T));
-            if (value is ushort s) return (T)Convert.ChangeType(s, typeof(T));
-            if (value is uint i) return (T)Convert.ChangeType(i, typeof(T));
-            if (value is ulong l) return (T)Convert.ChangeType(l, typeof(T));
-            
-            throw new NotSupportedException($"Conversion from {value.GetType()} to {typeof(T)} is not supported");
+            // 避免使用动态类型转换
+            if (typeof(T) == typeof(byte) && typeof(TP) == typeof(byte))
+                _value = (TP)(object)(byte)(object)value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(ushort))
+                _value = (TP)(object)(ushort)(object)value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(uint))
+                _value = (TP)(object)(uint)(object)value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(ulong))
+                _value = (TP)(object)(ulong)(object)value;
+            else if (typeof(T) == typeof(byte) && typeof(TP) == typeof(ushort))
+                _value = (TP)(object)(ushort)(byte)(object)value;
+            else if (typeof(T) == typeof(byte) && typeof(TP) == typeof(uint))
+                _value = (TP)(object)(uint)(byte)(object)value;
+            else if (typeof(T) == typeof(byte) && typeof(TP) == typeof(ulong))
+                _value = (TP)(object)(ulong)(byte)(object)value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(byte))
+                _value = (TP)(object)(byte)(ushort)(object)value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(uint))
+                _value = (TP)(object)(uint)(ushort)(object)value;
+            else if (typeof(T) == typeof(ushort) && typeof(TP) == typeof(ulong))
+                _value = (TP)(object)(ulong)(ushort)(object)value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(byte))
+                _value = (TP)(object)(byte)(uint)(object)value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(ushort))
+                _value = (TP)(object)(ushort)(uint)(object)value;
+            else if (typeof(T) == typeof(uint) && typeof(TP) == typeof(ulong))
+                _value = (TP)(object)(ulong)(uint)(object)value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(byte))
+                _value = (TP)(object)(byte)(ulong)(object)value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(ushort))
+                _value = (TP)(object)(ushort)(ulong)(object)value;
+            else if (typeof(T) == typeof(ulong) && typeof(TP) == typeof(uint))
+                _value = (TP)(object)(uint)(ulong)(object)value;
+            else
+                throw new NotSupportedException($"Conversion from {typeof(T)} to {typeof(TP)} is not supported in Value.Set");
         }
     }
 }
