71f5d30ce3f4442d866f522b3ce195cd775ab5c2
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-16 18:13:16 +0800

    Update OpAdd.cs

--- src/Ryujinx.HLE/HOS/Tamper/Operations/OpAdd.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Operations/OpAdd.cs
@@ -1,5 +1,5 @@
-using System;
 using Ryujinx.Common.Logging;
+using System;
 
 namespace Ryujinx.HLE.HOS.Tamper.Operations
 {
@@ -17,47 +17,51 @@ namespace Ryujinx.HLE.HOS.Tamper.Operations
         }
 
         public void Execute()
-{
-    T lhsValue = _lhs.Get<T>();
-    T rhsValue = _rhs.Get<T>();
-    T result;
-    
-    Logger.Debug?.Print(LogClass.TamperMachine, 
-        $"OpAdd.Execute: {lhsValue} + {rhsValue}");
-    
-    if (typeof(T) == typeof(byte))
-    {
-        byte lhsByte = (byte)(object)lhsValue;
-        byte rhsByte = (byte)(object)rhsValue;
-        result = (T)(object)(byte)(lhsByte + rhsByte);
-    }
-    else if (typeof(T) == typeof(ushort))
-    {
-        ushort lhsUShort = (ushort)(object)lhsValue;
-        ushort rhsUShort = (ushort)(object)rhsValue;
-        result = (T)(object)(ushort)(lhsUShort + rhsUShort);
-    }
-    else if (typeof(T) == typeof(uint))
-    {
-        uint lhsUInt = (uint)(object)lhsValue;
-        uint rhsUInt = (uint)(object)rhsValue;
-        result = (T)(object)(uint)(lhsUInt + rhsUInt);
-    }
-    else if (typeof(T) == typeof(ulong))
-    {
-        ulong lhsULong = (ulong)(object)lhsValue;
-        ulong rhsULong = (ulong)(object)rhsValue;
-        result = (T)(object)(ulong)(lhsULong + rhsULong);
-    }
-    else
-    {
-        throw new NotSupportedException($"Type {typeof(T)} is not supported for ADD operation");
-    }
-    
-    Logger.Debug?.Print(LogClass.TamperMachine, 
-        $"OpAdd.Execute: result = {result}");
-    
-    _destination.Set(result);
-}
+        {
+            try
+            {
+                T lhsValue = _lhs.Get<T>();
+                T rhsValue = _rhs.Get<T>();
+                
+                Logger.Debug?.Print(LogClass.TamperMachine, 
+                    $"OpAdd<{typeof(T).Name}>.Execute: {FormatValue(lhsValue)} + {FormatValue(rhsValue)}");
+                
+                // 使用 switch 表达式进行类型安全的加法运算
+                T result = typeof(T).Name switch
+                {
+                    "Byte" => (T)(object)(byte)((byte)(object)lhsValue + (byte)(object)rhsValue),
+                    "UInt16" => (T)(object)(ushort)((ushort)(object)lhsValue + (ushort)(object)rhsValue),
+                    "UInt32" => (T)(object)(uint)((uint)(object)lhsValue + (uint)(object)rhsValue),
+                    "UInt64" => (T)(object)(ulong)((ulong)(object)lhsValue + (ulong)(object)rhsValue),
+                    _ => throw new NotSupportedException($"Type {typeof(T)} is not supported for ADD operation")
+                };
+                
+                Logger.Debug?.Print(LogClass.TamperMachine, 
+                    $"OpAdd<{typeof(T).Name}>.Execute: result = {FormatValue(result)}");
+                
+                _destination.Set(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error?.Print(LogClass.TamperMachine, 
+                    $"OpAdd<{typeof(T).Name}>.Execute failed: {ex.Message}");
+                throw;
+            }
+        }
+        
+        // 格式化值为十六进制字符串
+        private string FormatValue<TValue>(TValue value) where TValue : unmanaged
+        {
+            if (typeof(TValue) == typeof(byte))
+                return $"0x{(byte)(object)value:X2}";
+            else if (typeof(TValue) == typeof(ushort))
+                return $"0x{(ushort)(object)value:X4}";
+            else if (typeof(TValue) == typeof(uint))
+                return $"0x{(uint)(object)value:X8}";
+            else if (typeof(TValue) == typeof(ulong))
+                return $"0x{(ulong)(object)value:X16}";
+            else
+                return value.ToString();
+        }
     }
 }
