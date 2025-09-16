d06da0a2f0427a6e98666da747dda8ad1b529f7b
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-16 18:04:45 +0800

    Update OpAdd.cs

--- src/Ryujinx.HLE/HOS/Tamper/Operations/OpAdd.cs
+++ src/Ryujinx.HLE/HOS/Tamper/Operations/OpAdd.cs
@@ -16,41 +16,47 @@ namespace Ryujinx.HLE.HOS.Tamper.Operations
         }
 
         public void Execute()
-        {
-            T lhsValue = _lhs.Get<T>();
-            T rhsValue = _rhs.Get<T>();
-            T result;
-            
-            if (typeof(T) == typeof(byte))
-            {
-                byte lhsByte = (byte)(object)lhsValue;
-                byte rhsByte = (byte)(object)rhsValue;
-                result = (T)(object)(byte)(lhsByte + rhsByte);
-            }
-            else if (typeof(T) == typeof(ushort))
-            {
-                ushort lhsUShort = (ushort)(object)lhsValue;
-                ushort rhsUShort = (ushort)(object)rhsValue;
-                result = (T)(object)(ushort)(lhsUShort + rhsUShort);
-            }
-            else if (typeof(T) == typeof(uint))
-            {
-                uint lhsUInt = (uint)(object)lhsValue;
-                uint rhsUInt = (uint)(object)rhsValue;
-                result = (T)(object)(uint)(lhsUInt + rhsUInt);
-            }
-            else if (typeof(T) == typeof(ulong))
-            {
-                ulong lhsULong = (ulong)(object)lhsValue;
-                ulong rhsULong = (ulong)(object)rhsValue;
-                result = (T)(object)(ulong)(lhsULong + rhsULong);
-            }
-            else
-            {
-                throw new NotSupportedException($"Type {typeof(T)} is not supported for ADD operation");
-            }
-            
-            _destination.Set(result);
-        }
+{
+    T lhsValue = _lhs.Get<T>();
+    T rhsValue = _rhs.Get<T>();
+    T result;
+    
+    Logger.Debug?.Print(LogClass.TamperMachine, 
+        $"OpAdd.Execute: {lhsValue} + {rhsValue}");
+    
+    if (typeof(T) == typeof(byte))
+    {
+        byte lhsByte = (byte)(object)lhsValue;
+        byte rhsByte = (byte)(object)rhsValue;
+        result = (T)(object)(byte)(lhsByte + rhsByte);
+    }
+    else if (typeof(T) == typeof(ushort))
+    {
+        ushort lhsUShort = (ushort)(object)lhsValue;
+        ushort rhsUShort = (ushort)(object)rhsValue;
+        result = (T)(object)(ushort)(lhsUShort + rhsUShort);
+    }
+    else if (typeof(T) == typeof(uint))
+    {
+        uint lhsUInt = (uint)(object)lhsValue;
+        uint rhsUInt = (uint)(object)rhsValue;
+        result = (T)(object)(uint)(lhsUInt + rhsUInt);
+    }
+    else if (typeof(T) == typeof(ulong))
+    {
+        ulong lhsULong = (ulong)(object)lhsValue;
+        ulong rhsULong = (ulong)(object)rhsValue;
+        result = (T)(object)(ulong)(lhsULong + rhsULong);
+    }
+    else
+    {
+        throw new NotSupportedException($"Type {typeof(T)} is not supported for ADD operation");
+    }
+    
+    Logger.Debug?.Print(LogClass.TamperMachine, 
+        $"OpAdd.Execute: result = {result}");
+    
+    _destination.Set(result);
+}
     }
 }
