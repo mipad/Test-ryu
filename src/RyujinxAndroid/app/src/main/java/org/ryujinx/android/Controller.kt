6c0325915c14b477a51f986402ec20f0911ef541
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-18 00:20:20 +0800

    Create Controller.kt

---
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/Controller.kt
@@ -0,0 +1,20 @@
+// Controller.kt
+package org.ryujinx.android
+
+data class Controller(
+    val id: String,
+    val name: String,
+    var controllerType: ControllerType = ControllerType.PRO_CONTROLLER,
+    val isVirtual: Boolean = false
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Controller
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
