a72ae62d028bf307d4a86f53d39d4959e71a2827
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-21 18:20:44 +0800

    Update TimeZoneViewModel.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/viewmodels/TimeZoneViewModel.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/viewmodels/TimeZoneViewModel.kt
@@ -5,18 +5,14 @@ import org.ryujinx.android.RyujinxNative
 
 class TimeZoneViewModel : ViewModel() {
     fun getTimeZoneList(): Array<String> {
-        return RyujinxNative.jnaInstance.deviceGetTimeZoneList()
+        return RyujinxNative.getTimeZoneList()
     }
     
     fun setTimeZone(timeZone: String) {
-        // 这里需要实现设置时区的逻辑
-        // 你可能需要在JNI层添加一个设置时区的方法
-        RyujinxNative.jnaInstance.setTimeZone(timeZone)
+        RyujinxNative.setTimeZone(timeZone)
     }
     
     fun getCurrentTimeZone(): String {
-        // 这里需要实现获取当前时区的逻辑
-        // 你可能需要在JNI层添加一个获取当前时区的方法
-        return RyujinxNative.jnaInstance.getCurrentTimeZone()
+        return RyujinxNative.getCurrentTimeZone()
     }
 }
