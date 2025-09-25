65cb592114ec62ef89f23d600773947ead5bc066
shuyi <54129575+mipad@users.noreply.github.com>
2025-09-26 00:34:03 +0800

    Update PlayerSetting.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/PlayerSetting.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/PlayerSetting.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PlayerSetting(
-    val playerNumber: Int,
-    val isConnected: Boolean,
-    var controllerType: Int // 0=Pro, 1=JoyCon L, 2=JoyCon R, 3=JoyCon Pair, 4=Handheld
+    val playerIndex: Int,      // 0-based 索引 (0-7)
+    var isConnected: Boolean,  // 是否连接
+    var controllerType: Int   // 0=Pro, 1=JoyCon L, 2=JoyCon R, 3=JoyCon Pair, 4=Handheld
 )
