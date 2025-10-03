2278554cbe37fde4b180505b813953b724c15b70
shuyi <54129575+mipad@users.noreply.github.com>
2025-10-04 03:02:00 +0800

    Update MainActivity.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/MainActivity.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/MainActivity.kt
@@ -1,14 +1,9 @@
 package org.ryujinx.android
 
 import android.annotation.SuppressLint
-import android.app.PictureInPictureParams
 import android.content.pm.ActivityInfo
-import android.content.res.Configuration
-import android.graphics.Rect
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.util.Rational
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.WindowManager
@@ -27,6 +22,7 @@ import org.ryujinx.android.viewmodels.MainViewModel
 import org.ryujinx.android.viewmodels.QuickSettings
 import org.ryujinx.android.views.MainView
 
+
 class MainActivity : BaseActivity() {
     private var physicalControllerManager: PhysicalControllerManager =
         PhysicalControllerManager(this)
@@ -36,10 +32,6 @@ class MainActivity : BaseActivity() {
     var isActive = false
     var storageHelper: SimpleStorageHelper? = null
     lateinit var uiHandler: UiHandler
-    
-    // 画中画相关变量
-    private var isInPictureInPictureMode = false
-    private val pipParamsBuilder by lazy { PictureInPictureParams.Builder() }
 
     companion object {
         var mainViewModel: MainViewModel? = null
@@ -187,109 +179,8 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    // 进入画中画模式
-    fun enterPictureInPictureMode() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && isGameRunning) {
-            try {
-                val rational = Rational(width, height)
-                pipParamsBuilder.setAspectRatio(rational)
-                
-                val params = pipParamsBuilder.build()
-                setPictureInPictureParams(params)
-                enterPictureInPictureMode(params)
-                isInPictureInPictureMode = true
-                
-                // 隐藏不必要的UI元素
-                setFullScreen(true)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    // 退出画中画模式
-    fun exitPictureInPictureMode() {
-        isInPictureInPictureMode = false
-        // 恢复UI状态
-        setFullScreen(true)
-    }
-
-    override fun onUserLeaveHint() {
-        super.onUserLeaveHint()
-        // 当用户点击Home键或切换到其他应用时，自动进入画中画模式
-        if (isGameRunning && !isInPictureInPictureMode) {
-            enterPictureInPictureMode()
-        }
-    }
-
-    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
-        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
-        this.isInPictureInPictureMode = isInPictureInPictureMode
-        
-        if (isInPictureInPictureMode) {
-            // 进入画中画模式时的处理
-            motionSensorManager.unregister() // 在画中画模式下禁用运动传感器
-            mainViewModel?.performanceManager?.setTurboMode(false) // 降低性能消耗
-        } else {
-            // 退出画中画模式时的处理
-            if (isActive && QuickSettings(this).enableMotion) {
-                motionSensorManager.register()
-            }
-            if (isGameRunning) {
-                mainViewModel?.performanceManager?.setTurboMode(true)
-            }
-            setFullScreen(true)
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        isActive = false
-
-        if (isGameRunning && !isInPictureInPictureMode) {
-            mainViewModel?.performanceManager?.setTurboMode(false)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        isActive = true
-
-        if (isGameRunning && !isInPictureInPictureMode) {
-            setFullScreen(true)
-            if (QuickSettings(this).enableMotion)
-                motionSensorManager.register()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        isActive = false
-
-        if (isGameRunning && !isInPictureInPictureMode) {
-            mainViewModel?.performanceManager?.setTurboMode(false)
-        }
-
-        if (!isInPictureInPictureMode) {
-            motionSensorManager.unregister()
-        }
-    }
-
     @SuppressLint("RestrictedApi")
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
-        // 在画中画模式下禁用部分输入
-        if (isInPictureInPictureMode) {
-            when (event.keyCode) {
-                KeyEvent.KEYCODE_BACK -> {
-                    if (event.action == KeyEvent.ACTION_UP) {
-                        // 在画中画模式下点击返回键可以退出画中画
-                        exitPictureInPictureMode()
-                    }
-                    return true
-                }
-            }
-        }
-        
         event.apply {
             if (physicalControllerManager.onKeyEvent(this))
                 return true
@@ -298,12 +189,40 @@ class MainActivity : BaseActivity() {
     }
 
     override fun dispatchGenericMotionEvent(ev: MotionEvent?): Boolean {
-        // 在画中画模式下禁用运动事件
-        if (!isInPictureInPictureMode) {
-            ev?.apply {
-                physicalControllerManager.onMotionEvent(this)
-            }
+        ev?.apply {
+            physicalControllerManager.onMotionEvent(this)
         }
         return super.dispatchGenericMotionEvent(ev)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        isActive = false
+
+        if (isGameRunning) {
+            mainViewModel?.performanceManager?.setTurboMode(false)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isActive = true
+
+        if (isGameRunning) {
+            setFullScreen(true)
+            if (QuickSettings(this).enableMotion)
+                motionSensorManager.register()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isActive = true
+
+        if (isGameRunning) {
+            mainViewModel?.performanceManager?.setTurboMode(false)
+        }
+
+        motionSensorManager.unregister()
     }
 }
