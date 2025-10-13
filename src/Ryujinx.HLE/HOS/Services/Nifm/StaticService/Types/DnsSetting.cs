f58c7eb5675df0cac05eaa0fe5ca1754884a84dc
shuyi <54129575+mipad@users.noreply.github.com>
2025-10-14 00:03:13 +0800

    Update DnsSetting.cs

--- src/Ryujinx.HLE/HOS/Services/Nifm/StaticService/Types/DnsSetting.cs
+++ src/Ryujinx.HLE/HOS/Services/Nifm/StaticService/Types/DnsSetting.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
@@ -17,28 +15,10 @@ namespace Ryujinx.HLE.HOS.Services.Nifm.StaticService.Types
 
         public DnsSetting(IPInterfaceProperties interfaceProperties)
         {
-            // 在 Android 平台上直接使用备用 DNS，不调用任何可能抛出异常的方法
-            if (OperatingSystem.IsAndroid())
-            {
-                IsDynamicDnsEnabled = false;
-                PrimaryDns = new IpV4Address(IPAddress.Parse("8.8.8.8"));
-                SecondaryDns = new IpV4Address(IPAddress.Parse("8.8.4.4"));
-                return;
-            }
-
-            // 原有逻辑保持不变
-            IsDynamicDnsEnabled = OperatingSystem.IsWindows() && interfaceProperties.IsDynamicDnsEnabled;
-
-            if (interfaceProperties.DnsAddresses.Count == 0)
-            {
-                PrimaryDns = new IpV4Address();
-                SecondaryDns = new IpV4Address();
-            }
-            else
-            {
-                PrimaryDns = new IpV4Address(interfaceProperties.DnsAddresses[0]);
-                SecondaryDns = new IpV4Address(interfaceProperties.DnsAddresses[interfaceProperties.DnsAddresses.Count > 1 ? 1 : 0]);
-            }
+            // 直接使用备用 DNS，不调用任何可能失败的方法
+            IsDynamicDnsEnabled = false;
+            PrimaryDns = new IpV4Address(IPAddress.Parse("8.8.8.8"));
+            SecondaryDns = new IpV4Address(IPAddress.Parse("8.8.4.4"));
         }
     }
 }
