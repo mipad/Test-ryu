746227c63b09fb04e8157b85525b46256bc5a2a6
shuyi <54129575+mipad@users.noreply.github.com>
2025-10-14 19:25:47 +0800

    Update NetworkViews.kt

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/NetworkViews.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/views/NetworkViews.kt
@@ -542,14 +542,6 @@ fun IdleLobbyView(networkViewModel: NetworkViewModel) {
                 }
             }
         }
-        
-        // 自动刷新大厅列表
-        LaunchedEffect(networkViewModel.isScanningLobbies.value) {
-            if (!networkViewModel.isScanningLobbies.value) {
-                delay(5000) // 5秒后自动刷新
-                networkViewModel.refreshLobbyList()
-            }
-        }
     }
 }
 
