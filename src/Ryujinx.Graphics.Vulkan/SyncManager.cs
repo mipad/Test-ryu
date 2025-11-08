b37e6d25e4230f8072c5839bdbf0375407c6dfdb
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-08 18:58:38 +0800

    Add files via upload

--- src/Ryujinx.Graphics.Vulkan/SyncManager.cs
+++ src/Ryujinx.Graphics.Vulkan/SyncManager.cs
@@ -8,12 +8,17 @@ namespace Ryujinx.Graphics.Vulkan
 {
     class SyncManager
     {
-        private class SyncHandle
+        internal class SyncHandle
         {
             public ulong ID;
             public MultiFenceHolder Waitable;
             public ulong FlushId;
             public bool Signalled;
+
+            public bool NeedsFlush(ulong currentFlushId)
+            {
+                return (long)(FlushId - currentFlushId) >= 0;
+            }
         }
 
         private ulong _firstHandle;
@@ -226,15 +231,6 @@ namespace Ryujinx.Graphics.Vulkan
             _waitTicks = 0;
 
             return result;
-        }
-    }
-
-    // 新增：为 SyncHandle 添加扩展方法
-    internal static class SyncHandleExtensions
-    {
-        public static bool NeedsFlush(this SyncManager.SyncHandle handle, ulong currentFlushId)
-        {
-            return (long)(handle.FlushId - currentFlushId) >= 0;
         }
     }
 }
