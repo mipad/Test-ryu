def7294a14e7fdbcbc81fe731e604cd8cbd271d8
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-09 19:32:12 +0800

    Update FFmpeg.cmake

--- src/RyujinxAndroid/libryujinx/libs/FFmpeg.cmake
+++ src/RyujinxAndroid/libryujinx/libs/FFmpeg.cmake
@@ -180,7 +180,7 @@ ExternalProject_Add(
     ffmpeg
     # 使用稳定版本而不是 master
     GIT_REPOSITORY              https://github.com/FFmpeg/FFmpeg.git
-    GIT_TAG                     n8.1-dev  # 使用稳定版本
+    GIT_TAG                     n7.1.2  # 使用稳定版本
     GIT_PROGRESS                1
     GIT_SHALLOW                 1
     UPDATE_COMMAND              ""
