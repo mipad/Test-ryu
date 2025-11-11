9ac8e1a81c046cf10b1283444d0f80c64c5d8393
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-12 01:10:33 +0800

    Update TranslationFlags.cs

--- src/Ryujinx.Graphics.Shader/Translation/TranslationFlags.cs
+++ src/Ryujinx.Graphics.Shader/Translation/TranslationFlags.cs
@@ -10,6 +10,5 @@ namespace Ryujinx.Graphics.Shader.Translation
         VertexA = 1 << 0,
         Compute = 1 << 1,
         DebugMode = 1 << 2,
-        Optimize = 1 << 3,  // 添加 Optimize 标志
     }
 }
