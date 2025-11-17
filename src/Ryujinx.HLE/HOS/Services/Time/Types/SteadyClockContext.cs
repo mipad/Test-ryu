ed78d7cb99fa9e54d51c5bda9d02a06eb1c6ed90
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-18 04:50:52 +0800

    还原

--- src/Ryujinx.HLE/HOS/Services/Time/Types/SteadyClockContext.cs
+++ src/Ryujinx.HLE/HOS/Services/Time/Types/SteadyClockContext.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 namespace Ryujinx.HLE.HOS.Services.Time.Types
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 4)]
-    public struct SteadyClockContext
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct SteadyClockContext
     {
         public ulong InternalOffset;
         public UInt128 ClockSourceId;
