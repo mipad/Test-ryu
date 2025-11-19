852c224b28c4d0760b16551144967101a79dc66a
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-20 05:48:35 +0800

    Add files via upload

--- src/ARMeilleure/Memory/ReservedRegion.cs
+++ src/ARMeilleure/Memory/ReservedRegion.cs
@@ -7,7 +7,8 @@ namespace ARMeilleure.Memory
         public const int DefaultGranularity = 65536; // Mapping granularity in Windows.
 
         public IJitMemoryBlock Block { get; }
-
+        public IJitMemoryAllocator Allocator { get; }
+        
         public IntPtr Pointer => Block.Pointer;
 
         private readonly ulong _maxSize;
@@ -20,7 +21,8 @@ namespace ARMeilleure.Memory
             {
                 granularity = DefaultGranularity;
             }
-
+            
+            Allocator = allocator;
             Block = allocator.Reserve(maxSize);
             _maxSize = maxSize;
             _sizeGranularity = granularity;
