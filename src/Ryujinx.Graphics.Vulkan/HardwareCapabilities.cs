08b7990906c5522e78f3b2ad43d54bff9d046e35
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-20 08:20:20 +0800

    Update HardwareCapabilities.cs

--- src/Ryujinx.Graphics.Vulkan/HardwareCapabilities.cs
+++ src/Ryujinx.Graphics.Vulkan/HardwareCapabilities.cs
@@ -31,7 +31,7 @@ namespace Ryujinx.Graphics.Vulkan
         public readonly bool SupportsShaderStorageImageMultisample;
         public readonly bool SupportsConditionalRendering;
         public readonly bool SupportsExtendedDynamicState;
-        public readonly bool SupportsExtendedDynamicState2; // 新增：扩展动态状态2支持
+        public readonly bool SupportsExtendedDynamicState2;
         public readonly bool SupportsMultiView;
         public readonly bool SupportsNullDescriptors;
         public readonly bool SupportsPushDescriptors;
@@ -49,10 +49,10 @@ namespace Ryujinx.Graphics.Vulkan
         public readonly bool SupportsDepthClipControl;
         public readonly bool SupportsAttachmentFeedbackLoop;
         public readonly bool SupportsDynamicAttachmentFeedbackLoop;
-        public readonly bool SupportsTimelineSemaphores; // 新增：时间线信号量支持
-        public readonly bool SupportsSynchronization2; // 新增：同步2支持
-        public readonly bool SupportsDynamicRendering; // 新增：动态渲染支持
-        public readonly bool SupportsTileOptimization; // 新增：Tile 优化支持
+        public readonly bool SupportsTimelineSemaphores;
+        public readonly bool SupportsSynchronization2;
+        public readonly bool SupportsDynamicRendering;
+        public readonly bool SupportsTileOptimization;
         public readonly uint SubgroupSize;
         public readonly SampleCountFlags SupportedSampleCounts;
         public readonly PortabilitySubsetFlags PortabilitySubset;
@@ -76,7 +76,7 @@ namespace Ryujinx.Graphics.Vulkan
             bool supportsShaderStorageImageMultisample,
             bool supportsConditionalRendering,
             bool supportsExtendedDynamicState,
-            bool supportsExtendedDynamicState2, // 新增参数
+            bool supportsExtendedDynamicState2,
             bool supportsMultiView,
             bool supportsNullDescriptors,
             bool supportsPushDescriptors,
@@ -94,10 +94,10 @@ namespace Ryujinx.Graphics.Vulkan
             bool supportsDepthClipControl,
             bool supportsAttachmentFeedbackLoop,
             bool supportsDynamicAttachmentFeedbackLoop,
-            bool supportsTimelineSemaphores, // 新增参数
-            bool supportsSynchronization2, // 新增参数
-            bool supportsDynamicRendering, // 新增参数
-            bool supportsTileOptimization, // 新增参数：Tile 优化支持
+            bool supportsTimelineSemaphores,
+            bool supportsSynchronization2,
+            bool supportsDynamicRendering,
+            bool supportsTileOptimization,
             uint subgroupSize,
             SampleCountFlags supportedSampleCounts,
             PortabilitySubsetFlags portabilitySubset,
@@ -120,7 +120,7 @@ namespace Ryujinx.Graphics.Vulkan
             SupportsShaderStorageImageMultisample = supportsShaderStorageImageMultisample;
             SupportsConditionalRendering = supportsConditionalRendering;
             SupportsExtendedDynamicState = supportsExtendedDynamicState;
-            SupportsExtendedDynamicState2 = supportsExtendedDynamicState2; // 新增赋值
+            SupportsExtendedDynamicState2 = supportsExtendedDynamicState2;
             SupportsMultiView = supportsMultiView;
             SupportsNullDescriptors = supportsNullDescriptors;
             SupportsPushDescriptors = supportsPushDescriptors;
@@ -138,10 +138,10 @@ namespace Ryujinx.Graphics.Vulkan
             SupportsDepthClipControl = supportsDepthClipControl;
             SupportsAttachmentFeedbackLoop = supportsAttachmentFeedbackLoop;
             SupportsDynamicAttachmentFeedbackLoop = supportsDynamicAttachmentFeedbackLoop;
-            SupportsTimelineSemaphores = supportsTimelineSemaphores; // 新增赋值
-            SupportsSynchronization2 = supportsSynchronization2; // 新增赋值
-            SupportsDynamicRendering = supportsDynamicRendering; // 新增赋值
-            SupportsTileOptimization = supportsTileOptimization; // 新增赋值：Tile 优化支持
+            SupportsTimelineSemaphores = supportsTimelineSemaphores;
+            SupportsSynchronization2 = supportsSynchronization2;
+            SupportsDynamicRendering = supportsDynamicRendering;
+            SupportsTileOptimization = supportsTileOptimization;
             SubgroupSize = subgroupSize;
             SupportedSampleCounts = supportedSampleCounts;
             PortabilitySubset = portabilitySubset;
@@ -151,3 +151,4 @@ namespace Ryujinx.Graphics.Vulkan
         }
     }
 }
+
