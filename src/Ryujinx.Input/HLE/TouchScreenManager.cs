9e54e655d24256b8210509e31698fb8ec7d63dfc
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-21 17:57:43 +0800

    Update TouchScreenManager.cs

--- src/Ryujinx.Input/HLE/TouchScreenManager.cs
+++ src/Ryujinx.Input/HLE/TouchScreenManager.cs
@@ -2,7 +2,6 @@ using Ryujinx.HLE;
 using Ryujinx.HLE.HOS.Services.Hid;
 using Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory.TouchScreen;
 using System;
-using System.Numerics;
 
 namespace Ryujinx.Input.HLE
 {
@@ -10,16 +9,7 @@ namespace Ryujinx.Input.HLE
     {
         private readonly IMouse _mouse;
         private Switch _device;
-        private Vector2 _lastPosition = Vector2.Zero;
-        private TouchState _touchState = TouchState.Released;
-        private bool _wasTouching = false;
-
-        public enum TouchState
-        {
-            Released,
-            Started,
-            Moving
-        }
+        private bool _wasClicking;
 
         public TouchScreenManager(IMouse mouse)
         {
@@ -31,76 +21,77 @@ namespace Ryujinx.Input.HLE
             _device = device;
         }
 
-        public void SetTouchPoint(int x, int y)
+        public bool Update(bool isFocused, bool isClicking = false, float aspectRatio = 0)
         {
-            _lastPosition = new Vector2(x, y);
-            
-            if (_touchState == TouchState.Released)
+            if (!isFocused || (!_wasClicking && !isClicking))
             {
-                _touchState = TouchState.Started;
-            }
-            else
-            {
-                _touchState = TouchState.Moving;
-            }
-        }
+                // In case we lost focus, send the end touch.
+                if (_wasClicking && !isClicking)
+                {
+                    MouseStateSnapshot snapshot = IMouse.GetMouseStateSnapshot(_mouse);
+                    var touchPosition = IMouse.GetScreenPosition(snapshot.Position, _mouse.ClientSize, aspectRatio);
 
-        public void ReleaseTouch()
-        {
-            _touchState = TouchState.Released;
-        }
+                    TouchPoint currentPoint = new()
+                    {
+                        Attribute = TouchAttribute.End,
 
-        public void Update(float aspectRatio = 0)
-        {
-            // 修复：移除 IsInvalid 检查，只检查是否为 null
-            if (_device?.Hid?.Touchscreen == null)
-                return;
-            
-            // 确保宽高比有效
-            if (aspectRatio <= 0) aspectRatio = 1.0f;
-            
-            // 使用正确的坐标转换方法
-            var touchPosition = IMouse.GetScreenPosition(_lastPosition, _mouse.ClientSize, aspectRatio);
-            
-            // 检查触摸是否在有效区域内
-            bool isValidTouch = touchPosition.X > 0 || touchPosition.Y > 0;
-            
-            // 处理触摸状态转换
-            switch (_touchState)
-            {
-                case TouchState.Started when isValidTouch:
-                    UpdateTouchPoint(touchPosition, TouchAttribute.Start);
-                    _touchState = TouchState.Moving;
-                    _wasTouching = true;
-                    break;
-                    
-                case TouchState.Moving when isValidTouch:
-                    UpdateTouchPoint(touchPosition, TouchAttribute.None);
-                    break;
-                    
-                case TouchState.Released when _wasTouching:
-                    UpdateTouchPoint(touchPosition, TouchAttribute.End);
-                    _wasTouching = false;
-                    break;
+                        X = (uint)touchPosition.X,
+                        Y = (uint)touchPosition.Y,
+
+                        // Placeholder values till more data is acquired
+                        DiameterX = 10,
+                        DiameterY = 10,
+                        Angle = 90,
+                    };
+
+                    _device.Hid.Touchscreen.Update(currentPoint);
+
+                }
+
+                _wasClicking = false;
+
+                _device.Hid.Touchscreen.Update();
+
+                return false;
             }
-            
-            // 强制更新 HID 状态
-            _device.Hid.Touchscreen.Update();
-        }
 
-        private void UpdateTouchPoint(Vector2 position, TouchAttribute attribute)
-        {
-            TouchPoint point = new TouchPoint
+            if (aspectRatio > 0)
             {
-                Attribute = attribute,
-                X = (uint)Math.Clamp(position.X, 0, 1280),
-                Y = (uint)Math.Clamp(position.Y, 0, 720),
-                DiameterX = 10,
-                DiameterY = 10,
-                Angle = 90
-            };
-            
-            _device.Hid.Touchscreen.Update(point);
+                MouseStateSnapshot snapshot = IMouse.GetMouseStateSnapshot(_mouse);
+                var touchPosition = IMouse.GetScreenPosition(snapshot.Position, _mouse.ClientSize, aspectRatio);
+
+                TouchAttribute attribute = TouchAttribute.None;
+
+                if (!_wasClicking && isClicking)
+                {
+                    attribute = TouchAttribute.Start;
+                }
+                else if (_wasClicking && !isClicking)
+                {
+                    attribute = TouchAttribute.End;
+                }
+
+                TouchPoint currentPoint = new()
+                {
+                    Attribute = attribute,
+
+                    X = (uint)touchPosition.X,
+                    Y = (uint)touchPosition.Y,
+
+                    // Placeholder values till more data is acquired
+                    DiameterX = 10,
+                    DiameterY = 10,
+                    Angle = 90,
+                };
+
+                _device.Hid.Touchscreen.Update(currentPoint);
+
+                _wasClicking = isClicking;
+
+                return true;
+            }
+
+            return false;
         }
 
         public void Dispose()
