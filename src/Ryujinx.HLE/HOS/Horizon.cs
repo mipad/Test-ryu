6805a1b72f0f63afbabb2132c1fff89dc712357f
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-21 17:33:51 +0800

    Add files via upload

--- src/Ryujinx.HLE/HOS/Horizon.cs
+++ src/Ryujinx.HLE/HOS/Horizon.cs
@@ -16,6 +16,8 @@ using Ryujinx.HLE.HOS.Services.Am.AppletAE.AllSystemAppletProxiesService.SystemA
 using Ryujinx.HLE.HOS.Services.Apm;
 using Ryujinx.HLE.HOS.Services.Caps;
 using Ryujinx.HLE.HOS.Services.Mii;
+using Ryujinx.HLE.HOS.Services.Nfc.AmiiboDecryption;
+using Ryujinx.HLE.HOS.Services.Nfc.Nfp;
 using Ryujinx.HLE.HOS.Services.Nfc.Nfp.NfpManager;
 using Ryujinx.HLE.HOS.Services.Nv;
 using Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostCtrl;
@@ -282,7 +284,7 @@ namespace Ryujinx.HLE.HOS
                 ProcessCreationInfo creationInfo = new("Service", 1, 0, 0x8000000, 1, Flags, 0, 0);
 
                 uint[] defaultCapabilities = {
-                    0x030363F7,
+                    (((uint)KScheduler.CpuCoresCount - 1) << 24) + (((uint)KScheduler.CpuCoresCount - 1) << 16) + 0x63F7u,
                     0x1FFFFFCF,
                     0x207FFFEF,
                     0x47E0060F,
@@ -337,11 +339,32 @@ namespace Ryujinx.HLE.HOS
 
         public void ScanAmiibo(int nfpDeviceId, string amiiboId, bool useRandomUuid)
         {
+            if (VirtualAmiibo.ApplicationBytes.Length > 0)
+            {
+                VirtualAmiibo.ApplicationBytes = Array.Empty<byte>();
+                VirtualAmiibo.InputBin = string.Empty;
+            }
             if (NfpDevices[nfpDeviceId].State == NfpDeviceState.SearchingForTag)
             {
                 NfpDevices[nfpDeviceId].State = NfpDeviceState.TagFound;
                 NfpDevices[nfpDeviceId].AmiiboId = amiiboId;
                 NfpDevices[nfpDeviceId].UseRandomUuid = useRandomUuid;
+            }
+        }
+        public void ScanAmiiboFromBin(string path)
+        {
+            VirtualAmiibo.InputBin = path;
+            if (VirtualAmiibo.ApplicationBytes.Length > 0)
+            {
+                VirtualAmiibo.ApplicationBytes = Array.Empty<byte>();
+            }
+            byte[] encryptedData = File.ReadAllBytes(path);
+            VirtualAmiiboFile newFile = AmiiboBinReader.ReadBinFile(encryptedData);
+            if (SearchingForAmiibo(out int nfpDeviceId))
+            {
+                NfpDevices[nfpDeviceId].State = NfpDeviceState.TagFound;
+                NfpDevices[nfpDeviceId].AmiiboId = newFile.AmiiboId;
+                NfpDevices[nfpDeviceId].UseRandomUuid = false;
             }
         }
 
