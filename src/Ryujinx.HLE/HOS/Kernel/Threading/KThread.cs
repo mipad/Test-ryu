8ddf1503c8cbe19f48a252cfa0e31a2f8fca4dcb
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-21 16:11:43 +0800

    Update KThread.cs

--- src/Ryujinx.HLE/HOS/Kernel/Threading/KThread.cs
+++ src/Ryujinx.HLE/HOS/Kernel/Threading/KThread.cs
@@ -114,7 +114,7 @@ namespace Ryujinx.HLE.HOS.Kernel.Threading
 
         public bool WaitingInArbitration { get; set; }
 
-        private readonly Lock _activityOperationLock = new();
+        private readonly object _activityOperationLock = new();
 
         public KThread(KernelContext context) : base(context)
         {
