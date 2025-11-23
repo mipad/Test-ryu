58857ddb285a5e1519a8025d6adc92f046bcbd2f
shuyi <54129575+mipad@users.noreply.github.com>
2025-11-24 07:15:59 +0800

    Update MemoryManager.cs

--- src/Ryujinx.Graphics.Gpu/Memory/MemoryManager.cs
+++ src/Ryujinx.Graphics.Gpu/Memory/MemoryManager.cs
@@ -213,7 +213,14 @@ namespace Ryujinx.Graphics.Gpu.Memory
 
                 size = Math.Min(data.Length, (int)PageSize - (int)(va & PageMask));
 
-                Physical.GetSpan(pa, size, tracked).CopyTo(data[..size]);
+                if (pa == PteUnmapped)
+                {
+                    data.Slice(0, size).Fill(0);
+                }
+                else
+                {
+                    Physical.GetSpan(pa, size, tracked).CopyTo(data[..size]);
+                }
 
                 offset += size;
             }
@@ -230,14 +237,7 @@ namespace Ryujinx.Graphics.Gpu.Memory
                 }
                 else
                 {
-                    if (pa == PteUnmapped)
-                {
-                    data.Slice(offset, size).Fill(0);
-                }
-                else
-                {
                     Physical.GetSpan(pa, size, tracked).CopyTo(data.Slice(offset, size));
-                }
                 }
             }
         }
