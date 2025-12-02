1abdbcfe647a340fbefb1d2e323a928327b962d8
shuyi <54129575+mipad@users.noreply.github.com>
2025-12-03 00:43:24 +0800

    Update KProcess.cs

--- src/Ryujinx.HLE/HOS/Kernel/Process/KProcess.cs
+++ src/Ryujinx.HLE/HOS/Kernel/Process/KProcess.cs
@@ -72,8 +72,8 @@ namespace Ryujinx.HLE.HOS.Kernel.Process
         private ulong _mainThreadStackSize;
         private ulong _memoryUsageCapacity;
 
-        // 改进1: 添加ASLR偏移常量
-        private const ulong AslrOffset32Bit = 0x500000;
+        // 注意：已移除AslrOffset32Bit常量，因为偏移量在ProcessLoaderHelper中添加
+        // private const ulong AslrOffset32Bit = 0x500000;
         
         public KHandleTable HandleTable { get; private set; }
 
@@ -246,14 +246,8 @@ namespace Ryujinx.HLE.HOS.Kernel.Process
 
             InitializeMemoryManager(creationInfo.Flags);
             
-            // 改进: 为32位进程添加ASLR偏移
+            // 注意：这里不再添加ASLR偏移，因为ProcessLoaderHelper已经添加过了
             ulong codeAddress = creationInfo.CodeAddress + Context.ReservedSize;
-            
-            if ((creationInfo.Flags & ProcessCreationFlags.AddressSpaceMask) == ProcessCreationFlags.AddressSpace32Bit ||
-                (creationInfo.Flags & ProcessCreationFlags.AddressSpaceMask) == ProcessCreationFlags.AddressSpace32BitWithoutAlias)
-            {
-                codeAddress += AslrOffset32Bit;
-            }
             
             ulong codeSize = codePagesCount * KPageTableBase.PageSize;
 
@@ -357,13 +351,8 @@ namespace Ryujinx.HLE.HOS.Kernel.Process
             Flags = creationInfo.Flags;
             TitleId = creationInfo.TitleId;
             
-            // 改进: 为32位地址空间添加ASLR偏移
+            // 注意：这里不再添加ASLR偏移，因为ProcessLoaderHelper已经添加过了
             _entrypoint = creationInfo.CodeAddress + Context.ReservedSize;
-            if ((Flags & ProcessCreationFlags.AddressSpaceMask) == ProcessCreationFlags.AddressSpace32Bit ||
-                (Flags & ProcessCreationFlags.AddressSpaceMask) == ProcessCreationFlags.AddressSpace32BitWithoutAlias)
-            {
-                _entrypoint += AslrOffset32Bit;
-            }
             
             _imageSize = (ulong)creationInfo.CodePagesCount * KPageTableBase.PageSize;
             
