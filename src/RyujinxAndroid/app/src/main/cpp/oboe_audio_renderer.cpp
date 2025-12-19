8dddc5a133708334a4377a989ed7fc1b90e7e908
shuyi <54129575+mipad@users.noreply.github.com>
2025-12-20 01:25:28 +0800

    Update oboe_audio_renderer.cpp

--- src/RyujinxAndroid/app/src/main/cpp/oboe_audio_renderer.cpp
+++ src/RyujinxAndroid/app/src/main/cpp/oboe_audio_renderer.cpp
@@ -56,7 +56,7 @@ void OboeAudioRenderer::Shutdown() {
 void OboeAudioRenderer::ConfigureForAAudioExclusive(oboe::AudioStreamBuilder& builder) {
     builder.setPerformanceMode(oboe::PerformanceMode::LowLatency)
            ->setAudioApi(oboe::AudioApi::AAudio)
-           ->setSharingMode(oboe::SharingMode::Exclusive)
+           ->setSharingMode(oboe::SharingMode::Shared)
            ->setDirection(oboe::Direction::Output)
            ->setSampleRate(m_sample_rate.load())
            ->setSampleRateConversionQuality(oboe::SampleRateConversionQuality::High)
