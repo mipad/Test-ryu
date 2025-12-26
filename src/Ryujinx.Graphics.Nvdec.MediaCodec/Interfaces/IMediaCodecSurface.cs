64bb8e7caf6b949727659ccb995dca6af6980fe6
shuyi <54129575+mipad@users.noreply.github.com>
2025-12-26 09:00:20 +0800

    Update IMediaCodecSurface.cs

--- src/Ryujinx.Graphics.Nvdec.MediaCodec/Interfaces/IMediaCodecSurface.cs
+++ src/Ryujinx.Graphics.Nvdec.MediaCodec/Interfaces/IMediaCodecSurface.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Ryujinx.Graphics.Nvdec.MediaCodec.Interfaces;
+
 
 namespace Ryujinx.Graphics.Nvdec.MediaCodec.Interfaces
 {
