db85cd951efe0f9b8549c3a285c81242d4397ada
shuyi <54129575+mipad@users.noreply.github.com>
2025-12-29 00:00:02 +0800

    Update NewStructs.cs

--- src/Ryujinx.Graphics.Nvdec.FFmpeg/Native/NewStructs.cs
+++ src/Ryujinx.Graphics.Nvdec.FFmpeg/Native/NewStructs.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace Ryujinx.Graphics.Nvdec.FFmpeg.Native
@@ -22,69 +23,9 @@ namespace Ryujinx.Graphics.Nvdec.FFmpeg.Native
     {
         public FFmpegApi.AVPixelFormat PixFmt;
         public FFmpegApi.AVHWDeviceType DeviceType;
-        public AVCodecHWConfigMethod Methods;
+        public int Methods;
         public int DeviceCaps;
         public unsafe byte* ConstraintSets;
         public int NumConstraintSets;
-    }
-    
-    [Flags]
-    internal enum AVCodecHWConfigMethod
-    {
-        AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX = 0x01,
-        AV_CODEC_HW_CONFIG_METHOD_HW_FRAMES_CTX = 0x02,
-        AV_CODEC_HW_CONFIG_METHOD_INTERNAL = 0x04,
-        AV_CODEC_HW_CONFIG_METHOD_AD_HOC = 0x08,
-    }
-    
-    // FFCodec结构体用于访问编解码器私有API
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct FFCodec<T> where T : unmanaged
-    {
-        public T Base;
-        public unsafe void* CodecCallback;
-    }
-    
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct FFCodecLegacy<T> where T : unmanaged
-    {
-        public T Base;
-        public unsafe void* Decode;
-    }
-    
-    // AVCodec的旧版本结构体
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct AVCodec501
-    {
-        public unsafe byte* Name;
-        public unsafe byte* LongName;
-        public int Type;
-        public AVCodecID Id;
-        public int Capabilities;
-        public unsafe byte** SupportedFramerates;
-        public unsafe int* PixFmts;
-        public unsafe int* SupportedSamplerates;
-        public unsafe ushort* SampleFmts;
-        public unsafe ulong* ChannelLayouts;
-        public byte MaxLowres;
-        public unsafe void* PrivClass;
-        public unsafe byte** Profiles;
-        public unsafe void* Next;
-        public unsafe void* InitThreadCopy;
-        public unsafe void* UpdateThreadContext;
-        public unsafe void* Defaults;
-        public unsafe void* InitStaticData;
-        public unsafe void* Init;
-        public unsafe void* EncodeSub;
-        public unsafe void* Encode2;
-        public unsafe void* Decode;
-        public unsafe void* Close;
-        public unsafe void* ReceiveFrame;
-        public unsafe void* ReceivePacket;
-        public int Flush;
-        public unsafe void* CapsInternal;
-        public unsafe byte* WrapperName;
-        public unsafe void* PrivDataSize;
-        public unsafe void* UpdateThreadContextForUser;
     }
 }
