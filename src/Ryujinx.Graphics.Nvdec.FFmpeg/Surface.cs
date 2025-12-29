39e4c20fe1aefd8364bb307302268d6a54dab1df
shuyi <54129575+mipad@users.noreply.github.com>
2025-12-29 13:39:29 +0800

    Update Surface.cs

--- src/Ryujinx.Graphics.Nvdec.FFmpeg/Surface.cs
+++ src/Ryujinx.Graphics.Nvdec.FFmpeg/Surface.cs
@@ -11,8 +11,6 @@ namespace Ryujinx.Graphics.Nvdec.FFmpeg
         public int RequestedWidth { get; }
         public int RequestedHeight { get; }
         
-        public int PixelFormat => Frame->Format;
-
         public Plane YPlane => new((IntPtr)Frame->Data[0], Stride * Height);
         public Plane UPlane => new((IntPtr)Frame->Data[1], UvStride * UvHeight);
         public Plane VPlane => new((IntPtr)Frame->Data[2], UvStride * UvHeight);
@@ -25,12 +23,6 @@ namespace Ryujinx.Graphics.Nvdec.FFmpeg
         public int UvWidth => (Width + 1) >> 1;
         public int UvHeight => (Height + 1) >> 1;
         public int UvStride => Frame->LineSize[1];
-        
-        // 格式检查
-        public bool IsYUV420P => PixelFormat == (int)FFmpegApi.AVPixelFormat.AV_PIX_FMT_YUV420P;
-        public bool IsNV12 => PixelFormat == (int)FFmpegApi.AVPixelFormat.AV_PIX_FMT_NV12;
-        public bool IsNV21 => PixelFormat == (int)FFmpegApi.AVPixelFormat.AV_PIX_FMT_NV21;
-        public bool IsMediaCodec => PixelFormat == (int)FFmpegApi.AVPixelFormat.AV_PIX_FMT_MEDIACODEC;
 
         public Surface(int width, int height)
         {
