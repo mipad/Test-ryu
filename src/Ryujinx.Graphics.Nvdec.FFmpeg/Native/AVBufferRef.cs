a049ebd950297be4c6aee4f95b93442d44057eb9
shuyi <54129575+mipad@users.noreply.github.com>
2025-12-31 12:58:41 +0800

    Update AVBufferRef.cs

--- src/Ryujinx.Graphics.Nvdec.FFmpeg/Native/AVBufferRef.cs
+++ src/Ryujinx.Graphics.Nvdec.FFmpeg/Native/AVBufferRef.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace Ryujinx.Graphics.Nvdec.FFmpeg.Native
 {
+    // 将 AVBuffer 设为 public
+    public unsafe struct AVBuffer
+    {
+        // 内部结构，通常不直接访问
+    }
+
     unsafe struct AVBufferRef
     {
 #pragma warning disable CS0649
@@ -9,10 +15,5 @@ namespace Ryujinx.Graphics.Nvdec.FFmpeg.Native
         public byte* Data;
         public int Size;
 #pragma warning restore CS0649
-    }
-
-    struct AVBuffer
-    {
-        // 内部结构，通常不直接访问
     }
 }
