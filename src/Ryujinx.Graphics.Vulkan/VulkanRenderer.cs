0ef6950e9cd882c3b1e4f35b24c9808b02f4dd8c
shuyi <54129575+mipad@users.noreply.github.com>
2026-01-15 20:47:25 +0800

    Update VulkanRenderer.cs

--- src/Ryujinx.Graphics.Vulkan/VulkanRenderer.cs
+++ src/Ryujinx.Graphics.Vulkan/VulkanRenderer.cs
@@ -1249,11 +1249,6 @@ public void PreFrame()
     // 刷新待处理的批量信号
     FlushPendingBatches();
     
-    // TBDR平台：预优化批量查询
-    if (IsTBDR && _pipeline != null)
-    {
-        _pipeline.OptimizeBatchQueries();
-    }
 }
 
         public ICounterEvent ReportCounter(CounterType type, EventHandler<ulong> resultHandler, float divisor, bool hostReserved)
